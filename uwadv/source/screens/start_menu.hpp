7de5872a1b52d07b3a2c1fa11a6e1e482f90ec3b
vividos <>
2002-06-25 17:31:29 +0000

    added start menu screen

---
+++ uwadv/source/screens/start_menu.hpp
@@ -0,0 +1,71 @@
+/*
+   Underworld Adventures - an Ultima Underworld hacking project
+   Copyright (c) 2002 Michael Fink
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+   $Id$
+
+*/
+/*! \file start_menu.hpp
+
+   start menu screen
+
+   shows the menu where the user can select "introduction", "create
+   character", "acknowledgements" or "journey onward" (if a game can
+   be loaded).
+
+*/
+
+// include guard
+#ifndef __uwadv_start_menu_hpp_
+#define __uwadv_start_menu_hpp_
+
+// needed includes
+#include "screen.hpp"
+
+
+// classes
+
+//! start menu screen class
+class ua_start_menu_screen: public ua_ui_screen_base
+{
+public:
+   //! ctor
+   ua_start_menu_screen(){}
+   //! dtor
+   virtual ~ua_start_menu_screen(){}
+
+   // virtual functions from ua_ui_screen_base
+
+   virtual void init();
+   virtual void done();
+   virtual void handle_event(SDL_Event &event);
+   virtual void render();
+   virtual void tick();
+
+protected:
+
+   ua_image img;
+   ua_texture tex;
+
+   //! current animation frame
+   unsigned int curframe;
+
+   //! animation frame count
+   unsigned int animcount;
+};
+
+#endif
