d45a780970ebf34874afefb9ed9f892876937a93
vividos <>
2002-07-22 19:03:10 +0000

    added initial ua_keymap implementation

---
+++ uwadv/source/keymap.cpp
@@ -0,0 +1,55 @@
+/*
+   Underworld Adventures - an Ultima Underworld hacking project
+   Copyright (c) 2002 Michael Fink
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+   $Id$
+
+*/
+/*! \file keymap.cpp
+
+   \brief keyboard key mappings
+
+*/
+
+// needed includes
+#include "common.hpp"
+#include "keymap.hpp"
+
+
+// ua_keymap methods
+
+ua_keymap::ua_keymap()
+{
+}
+
+void ua_keymap::init(ua_settings &settings)
+{
+   keymap.insert(std::make_pair<ua_key_value,unsigned int>(ua_key_forward,SDLK_w));
+   keymap.insert(std::make_pair<ua_key_value,unsigned int>(ua_key_turn_left,SDLK_a));
+   keymap.insert(std::make_pair<ua_key_value,unsigned int>(ua_key_turn_right,SDLK_d));
+}
+
+unsigned int ua_keymap::get_key(ua_key_value key)
+{
+   std::map<ua_key_value,unsigned int>::iterator iter =
+      keymap.find(key);
+
+   if (iter==keymap.end())
+      throw ua_exception("unknown keymap value queried");
+
+   return iter->second;
+}
---
+++ uwadv/source/keymap.hpp
@@ -0,0 +1,70 @@
+/*
+   Underworld Adventures - an Ultima Underworld hacking project
+   Copyright (c) 2002 Michael Fink
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+   $Id$
+
+*/
+/*! \file keymap.hpp
+
+   \brief keyboard key mappings
+
+*/
+
+// include guard
+#ifndef __uwadv_keymap_hpp_
+#define __uwadv_keymap_hpp_
+
+// needed includes
+#include "settings.hpp"
+#include "SDL_keysym.h"
+#include <map>
+
+
+// enums
+
+//! logical key values
+enum ua_key_value
+{
+   ua_key_forward=0,
+   ua_key_turn_left,
+   ua_key_turn_right,
+};
+
+
+// classes
+
+//! keymap class
+class ua_keymap
+{
+public:
+   //! ctor
+   ua_keymap();
+
+   //! initializes keymap
+   void init(ua_settings &settings);
+
+   //! returns SDL keysym value for a given key
+   unsigned int get_key(ua_key_value key);
+
+protected:
+   //! mapping from key value to SDL keysym value
+   std::map<ua_key_value,unsigned int> keymap;
+};
+
+
+#endif
