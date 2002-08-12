24cc04cce3fc2afaf6d7082093ffcb67b0901d7c
vividos <>
2002-08-12 18:45:58 +0000

    added constants file

---
+++ uwadv/uadata/uw1/scripts/uwinterface.lua
@@ -0,0 +1,93 @@
+--
+-- Underworld Adventures - an Ultima Underworld hacking project
+-- Copyright (c) 2002 Michael Fink
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+-- $Id$
+--
+
+--
+-- uwinterface.lua - lua underworld script; interface constants
+--
+
+-- constants
+
+-- player constants
+-- these should be in sync with the enums in player.hpp
+
+-- attributes
+player_attr_gender = 0
+player_attr_handedness = 1
+player_attr_appearance = 2
+player_attr_profession = 3
+
+player_attr_maplevel = 4
+
+player_attr_strength = 5
+player_attr_dexterity = 6
+player_attr_intelligence = 7
+player_attr_attack = 8
+player_attr_defense = 9
+
+player_attr_life = 10
+player_attr_max_life = 11
+player_attr_mana = 12
+player_attr_max_mana = 13
+
+player_attr_weariness = 14
+player_attr_hungriness = 15
+player_attr_poisoned = 16
+
+player_attr_talks = 17
+player_attr_kills = 18
+player_attr_level = 19
+player_attr_exp_points = 20
+
+
+-- skills
+player_skill_unarmed = 0
+player_skill_sword = 1
+player_skill_axe = 2
+player_skill_mace = 3
+player_skill_missile = 4
+player_skill_lore = 5
+player_skill_casting = 6
+player_skill_traps = 7
+player_skill_search = 8
+player_skill_track = 9
+player_skill_sneak = 10
+player_skill_repair = 11
+player_skill_charm = 12
+player_skill_picklock = 13
+player_skill_acrobat = 14
+player_skill_appraise = 15
+player_skill_swimming = 16
+
+
+-- tilemap constants
+-- these should be in sync with the enums in level.hpp
+
+-- tilemap types
+tilemap_type_solid = 0
+tilemap_type_open = 1
+tilemap_type_diagonal_se = 2
+tilemap_type_diagonal_sw = 3
+tilemap_type_diagonal_nw = 4
+tilemap_type_diagonal_ne = 5
+tilemap_type_slope_n = 6
+tilemap_type_slope_e = 7
+tilemap_type_slope_s = 8
+tilemap_type_slope_w = 9
