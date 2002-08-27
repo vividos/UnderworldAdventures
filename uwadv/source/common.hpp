52f4762d463a7854f4aa3a26c313a89e6e066f30
vividos <>
2002-08-27 16:32:09 +0000

    enclosed #pragma's with proper #ifdef

--- uwadv/source/common.hpp
+++ uwadv/source/common.hpp
@@ -35,7 +35,7 @@
 
 
 // get rid of warnings
-#ifdef WIN32
+#ifdef _MSC_VER
 # pragma warning( disable : 4786 ) // identifier was truncated to '255' characters in the debug information
 # pragma warning( disable : 4290 ) // C++ Exception Specification ignored
 #endif
