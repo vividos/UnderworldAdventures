193f14654832e45d064c7a27a88326b38246b261
vividos <>
2002-09-10 17:06:10 +0000

    moved object combine function to inventory.lua

--- uwadv/uadata/uw1/scripts/inventory.lua
+++ uwadv/uadata/uw1/scripts/inventory.lua
@@ -26,13 +26,85 @@
 -- constants
 
 -- inventory item category constants
-inv_cat_normal = 0   -- normal inventory object
-inv_cat_ring = 1     -- a ring that can be put on a finger
-inv_cat_legs = 2     -- armour that can be worn on the legs
-inv_cat_chest = 3    -- armour that can be worn on the chest
-inv_cat_hands = 4    -- armour that can be worn on the hands
-inv_cat_feet = 5     -- all sorts of boots
-inv_cat_head = 6     -- all sorts of helmets/crowns
+lua_inv_cat_normal = 0   -- normal inventory object
+lua_inv_cat_ring = 1     -- a ring that can be put on a finger
+lua_inv_cat_legs = 2     -- armour that can be worn on the legs
+lua_inv_cat_chest = 3    -- armour that can be worn on the chest
+lua_inv_cat_hands = 4    -- armour that can be worn on the hands
+lua_inv_cat_feet = 5     -- all sorts of boots
+lua_inv_cat_head = 6     -- all sorts of helmets/crowns
+
+-- combine result values
+lua_inv_cmb_failed = 0       -- items couldn't be combined
+lua_inv_cmb_dstr_first = 1   -- first (dropped) item is destroyed
+lua_inv_cmb_dstr_second = 2  -- second (dropped) item is destroyed
+lua_inv_cmb_dstr_both = 3    -- both the two objects are destroyed
+
+
+-- globals
+
+inv_cmb_recurse_flag = 0
+
+
+-- tables
+
+-- table with all possible object combinations
+-- objects that are destroyed when combined are marked with (*)
+inv_cmb_list = {
+
+   -- misc. objects
+
+   -- 0x0095 + 0x0116(*) = 0x0115
+   -- "a lit torch" + "a block of incense" = "a block of burning incense"
+   { item_id1  =          9*16 +  5, item_id2 = 1*256 +  1*16 +  6,
+     result_id = 1*256 +  1*16 +  5, success_code = lua_obj_cmb_dstr_second },
+
+   -- 0x0095 + 0x00b4(*) = 0x00b7
+   -- "a lit torch" + "an ear of corn" = "some_popcorn"
+   { item_id1  =          9*16 +  5, item_id2 =         11*16 +  4,
+     result_id =         11*16 +  7, success_code = lua_obj_cmb_dstr_second },
+
+   -- 0x011c(*) + 0x00d8(*) = 0x012b
+   -- "some_strong thread" + "a pole" = "a fishing pole"
+   { item_id1  = 1*256 +  1*16 + 12, item_id2 =         13*16 +  8,
+     result_id = 1*256 +  2*16 + 11, success_code = lua_obj_cmb_dstr_both },
+
+   -- key of infinity
+
+   -- 0x00e1(*) + 0x00e2(*) = 0x00e6
+   -- "the Key of Truth" + "the Key of Love" = "a two part key"
+   { item_id1  =         14*16 +  1, item_id2 =         14*16 +  2,
+     result_id =         14*16 +  6, success_code = lua_obj_cmb_dstr_both },
+
+   -- 0x00e1(*) + 0x00e3(*) = 0x00e4
+   -- "the Key of Truth" + "the Key of Courage" = "a two part key"
+   { item_id1  =         14*16 +  1, item_id2 =         14*16 +  3,
+     result_id =         14*16 +  4, success_code = lua_obj_cmb_dstr_both },
+
+   -- 0x00e2(*) + 0x00e3(*) = 0x00e5
+   -- "the Key of Love" + "the Key of Courage" = "a two part key"
+   { item_id1  =         14*16 +  2, item_id2 =         14*16 +  3,
+     result_id =         14*16 +  5, success_code = lua_obj_cmb_dstr_both },
+
+   -- 0x00e1(*) + 0x00e5(*) = 0x00e7
+   -- "the Key of Truth" + "a two part key" = "the Key of Infinity"
+   { item_id1  =         14*16 +  1, item_id2 =         14*16 +  5,
+     result_id =         14*16 +  7, success_code = lua_obj_cmb_dstr_both },
+
+   -- 0x00e2(*) + 0x00e4(*) = 0x00e7
+   -- "the Key of Love" + "a two part key" = "the Key of Infinity"
+   { item_id1  =         14*16 +  2, item_id2 =         14*16 +  4,
+     result_id =         14*16 +  7, success_code = lua_obj_cmb_dstr_both },
+
+   -- 0x00e3(*) + 0x00e6(*) = 0x00e7
+   -- "the Key of Courage" + "a two part key" = "the Key of Infinity"
+   { item_id1  =         14*16 +  3, item_id2 =         14*16 +  6,
+     result_id =         14*16 +  7, success_code = lua_obj_cmb_dstr_both },
+
+
+   -- stop entry
+   { item_id1  = -1, item_id2 = -1, result_id = -1, success_code = 0 },
+}
 
 
 -- functions
@@ -49,6 +121,7 @@ function lua_inventory_is_container(item_id)
 
    return ret
 end
+
 
 -- categorizes item by item id
 function lua_inventory_categorize_item(item_id)
@@ -93,4 +166,61 @@ function lua_inventory_categorize_item(item_id)
    end
 
    return type
+end
+
+
+-- prints "look" description
+function lua_inventory_look(item_id)
+   ui_print_string("lua_inventory_look(): look not implemented yet!")
+end
+
+
+-- prints "use" description
+function lua_inventory_use(item_id)
+   ui_print_string("lua_inventory_use(): look not implemented yet!")
+end
+
+
+-- tries to combine items
+function lua_inventory_combine_obj(item_id1, item_id2)
+
+   -- initial return values
+   success_code = lua_inv_cmb_failed
+   result_id = 0
+
+   -- check for all combinable items in table
+   idx = 1;
+   while inv_cmb_list[idx].item_id1 ~= -1 and success_code == lua_inv_cmb_failed
+   do
+
+      if inv_cmb_list[idx].item_id1 == item_id1 and
+         inv_cmb_list[idx].item_id2 == item_id2
+      then
+         -- found the right id
+         result_id = inv_cmb_list[idx].result_id
+         success_code = inv_cmb_list[idx].success_code
+      end
+
+      idx = idx + 1
+   end
+
+   -- check for combining objects using swapped item id's
+   if success_code == lua_inv_cmb_failed and inv_cmb_recurse_flag ~= 1
+   then
+
+      inv_cmb_recurse_flag = 1
+
+      -- check with swapped item id's
+      success_code, result_id = lua_inventory_combine_obj(item_id2, item_id1)
+
+      inv_cmb_recurse_flag = 0
+
+      -- revers success code when only one item is destroyed
+      if success_code == lua_inv_cmb_dstr_second
+      then
+         success_code = lua_inv_cmb_dstr_first
+      end
+   end
+
+   return success_code, result_id
 end
