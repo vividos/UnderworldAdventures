d482f17831433492ac2df3789c67ce20cb2053ea
vividos <>
2002-10-16 13:10:45 +0000

    fixed linux debug prototype (hopefully)

--- uwadv/source/linux/debug.cpp
+++ uwadv/source/linux/debug.cpp
@@ -21,48 +21,87 @@
 */
 /*! \file debug.cpp
 
-   \brief debug interface
+   \brief linux debug interface
 
 */
 
 // needed includes
 #include "common.hpp"
 #include "debug.hpp"
+#include "debug/uwaccess.hpp"
+#include <SDL_thread.h>
 
 
 // debugger implementation class
 
-class ua_debug_impl: public ua_debug_interface
+class ua_debug_impl_linux: public ua_debug_interface
 {
 public:
-   ua_debug_impl();
-   virtual ~ua_debug_impl();
+   ua_debug_impl_linux(ua_underworld* underw);
+   virtual ~ua_debug_impl_linux();
 
-   void start_debugger();
+   // implemented abstraction methods from ua_debug_interface
+   virtual void start_debugger();
+   virtual void lock();
+   virtual void unlock();
+   virtual ua_underworld* get_underworld();
+   virtual ua_uw_access_api* get_access_api();
+
+protected:
+   //! underworld object
+   ua_underworld* underw;
+
+   //! access api
+   ua_uw_access_api api;
 };
 
 
 // ua_debug_interface methods
 
-ua_debug_interface* ua_debug_interface::get_new_debug_interface()
+ua_debug_interface* ua_debug_interface::get_new_debug_interface(
+   ua_underworld* underw)
 {
-   return new ua_debug_impl;
+   return new ua_debug_impl_linux(underw);
 }
 
 
 // ua_debug_impl methods
 
-ua_debug_impl::ua_debug_impl()
+ua_debug_impl_linux::ua_debug_impl_linux(ua_underworld* theunderw)
+:underw(theunderw)
 {
-   // load debugger library
+   // todo: load dynamic debugger library
+   ua_trace("linux debug impl. started; debugger is not available\n");
+
+   // initialize access api
+   api.init();
 }
 
-ua_debug_impl::~ua_debug_impl()
+ua_debug_impl_linux::~ua_debug_impl_linux()
 {
-   // call uadebug_done() and free debugger library
 }
 
-void ua_debug_impl::start_debugger()
+void ua_debug_impl_linux::start_debugger()
 {
-   // call uadebug_start()
+   // todo: check if debugger already runs, and when not,
+   //       call uadebug_start() in dynamic library via own thread
+   ua_trace("starting uadebug thread: not implemented yet\n");
+}
+
+void ua_debug_impl_linux::lock()
+{
+}
+
+void ua_debug_impl_linux::unlock()
+{
+}
+
+ua_underworld* ua_debug_impl_linux::get_underworld()
+{
+   return underw;
+}
+
+ua_uw_access_api* ua_debug_impl_linux::get_access_api()
+{
+   return &api;
 }
