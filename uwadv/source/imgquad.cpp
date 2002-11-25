15be8586974534be95ca923859be26fec8bbeed6
vividos <>
2002-11-25 20:12:30 +0000

    added general image quad class

---
+++ uwadv/source/imgquad.cpp
@@ -0,0 +1,117 @@
+/*
+   Underworld Adventures - an Ultima Underworld hacking project
+   Copyright (c) 2002 Michael Fink
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+   $Id$
+
+*/
+/*! \file imgquad.cpp
+
+   \brief image quad implementation
+
+*/
+
+// needed includes
+#include "common.hpp"
+#include "imgquad.hpp"
+
+
+// ua_image_quad methods
+
+void ua_image_quad::init(ua_texture_manager* texmgr, unsigned int myxpos,
+   unsigned int myypos, unsigned int mywidth, unsigned int myheight)
+{
+   xpos = myxpos;
+   ypos = myypos;
+   mywidth += (mywidth&1); // even width
+
+   // init image
+   ua_image::create(mywidth,myheight,0,0);
+
+   // init texture
+   split_textures = mywidth>254;
+
+   tex.init(texmgr,split_textures ? 2 : 1,
+      GL_LINEAR,GL_LINEAR,GL_CLAMP,GL_CLAMP);
+}
+
+void ua_image_quad::convert_upload()
+{
+   // upload image
+   if (split_textures)
+   {
+      // split text image into two
+      ua_image img_split1,img_split2;
+
+      unsigned int texwidth = ua_image::xres/2+1;
+
+      copy_rect(img_split1,0,0, texwidth,ua_image::yres);
+      copy_rect(img_split2,texwidth-1,0, texwidth-1,ua_image::yres);
+
+      // upload it to the texture
+      tex.convert(img_split1,0);
+      tex.use(0);
+      tex.upload();
+
+      tex.convert(img_split2,1);
+      tex.use(1);
+      tex.upload();
+   }
+   else
+   {
+      // image is small enough
+      tex.convert(*this);
+      tex.use(0);
+      tex.upload();
+   }
+}
+
+void ua_image_quad::render()
+{
+   double u = tex.get_tex_u(), v = tex.get_tex_v();
+   tex.use(0);
+
+   unsigned int quadwidth = split_textures ? ua_image::xres/2+2 : ua_image::xres+2;
+   unsigned int quadheight = ua_image::yres;
+   double dx = split_textures ? 0.5/quadwidth : 0.0;
+
+   // render (first) quad
+   glBegin(GL_QUADS);
+   glTexCoord2d(0.0,  v);   glVertex2i(xpos+0,        200-ypos-quadheight);
+   glTexCoord2d(u-dx, v);   glVertex2i(xpos+quadwidth,200-ypos-quadheight);
+   glTexCoord2d(u-dx, 0.0); glVertex2i(xpos+quadwidth,200-ypos);
+   glTexCoord2d(0.0,  0.0); glVertex2i(xpos+0,        200-ypos);
+   glEnd();
+
+   if (split_textures)
+   {
+      tex.use(1);
+
+      // render second quad
+      glBegin(GL_QUADS);
+      glTexCoord2d(0.0, v  ); glVertex2i(xpos+quadwidth,  200-ypos-quadheight);
+      glTexCoord2d(u  , v  ); glVertex2i(xpos+quadwidth*2,200-ypos-quadheight);
+      glTexCoord2d(u  , 0.0); glVertex2i(xpos+quadwidth*2,200-ypos);
+      glTexCoord2d(0.0, 0.0); glVertex2i(xpos+quadwidth,  200-ypos);
+      glEnd();
+   }
+}
+
+void ua_image_quad::done()
+{
+   tex.done();
+}
---
+++ uwadv/source/imgquad.hpp
@@ -0,0 +1,69 @@
+/*
+   Underworld Adventures - an Ultima Underworld hacking project
+   Copyright (c) 2002 Underworld Adventures Team
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+   $Id$
+
+*/
+/*! \file imgquad.hpp
+
+   \brief image quad that can be drawn onto screen
+
+   the image quad supports resolutions up to 512 x 256
+
+*/
+
+// include guard
+#ifndef __uwadv_imgquad_hpp_
+#define __uwadv_imgquad_hpp_
+
+// needed includes
+#include "image.hpp"
+#include "texture.hpp"
+
+
+class ua_image_quad: public ua_image
+{
+public:
+   //! ctor
+   ua_image_quad(){}
+
+   //! initializes image quad
+   void init(ua_texture_manager* texmgr, unsigned int xpos, unsigned int ypos,
+      unsigned int width, unsigned int height);
+
+   //! converts and uploads the image to texture
+   void convert_upload();
+
+   //! renders image quad
+   void render();
+
+   //! cleans up image quad
+   void done();
+
+protected:
+   //! image quad texture
+   ua_texture tex;
+
+   //! starting pos on screen
+   unsigned int xpos, ypos;
+
+   //! indicates if we use two textures to draw the quad
+   bool split_textures;
+};
+
+#endif
