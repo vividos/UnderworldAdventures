1c05596e26a75f46919075d5bff89ceba0636fef
vividos <>
2002-11-25 00:35:58 +0000

    added unpacking and dumping of uw2 cnv.ark (not quite correct yet)

--- uwadv/hacking/cnvdec/cnvdec.cpp
+++ uwadv/hacking/cnvdec/cnvdec.cpp
@@ -133,11 +133,8 @@ struct
    { "OPNEG", 0, "", "negate, s[0] = -s[0]" },
 };
 
-void decode_instructions(FILE *fd, FILE *dest,unsigned int codesize)
+void decode_instructions(unsigned short* allcodes, FILE *dest,unsigned int codesize)
 {
-   unsigned short *allcodes = new unsigned short[codesize+1];
-   fread(allcodes,2,codesize,fd);
-
    fprintf(dest,";\n; code section\n;\n");
    for(unsigned int i=0; i<codesize; i++)
    {
@@ -171,8 +168,6 @@ void decode_instructions(FILE *fd, FILE *dest,unsigned int codesize)
 
       i += instructions[code].args;
    }
-
-   delete[] allcodes;
 }
 
 
@@ -200,7 +195,7 @@ int main()
    unsigned int *offsets = new unsigned int[entries];
    fread(offsets,4,entries,fd);
 
-   int slotcount = 0;
+   unsigned int convcount = 0;
 
    for(int i=0; i<entries; i++)
    {
@@ -211,6 +206,129 @@ int main()
       }
 
       fseek(fd,offsets[i],SEEK_SET);
+
+#ifdef HAVE_UW2
+
+      unsigned int length;
+      fread(&length,1,4,fd);
+
+      fprintf(dest,"uncompressed length: %04x bytes\n",length);
+
+      unsigned char* buffer = new unsigned char[length];
+
+      for(unsigned int n=0; n<length;)
+      {
+         unsigned char bits = fgetc(fd);
+
+         for(int b=0; b<8; b++, bits>>=1)
+         {
+            if (bits&1)
+            {
+               // bit set, copy one byte
+               buffer[n++] = fgetc(fd);
+            }
+            else
+            {
+               // bit cleared, "repeat run"
+               int m1 = fgetc(fd);
+               int m2 = fgetc(fd);
+
+               m1 |= (m2&0xF0) << 4;
+               if(m1&0x800)
+                  m1 |= 0xFFFFF000;
+
+               m2 =  (m2&0x0F) + 3;
+               m1 += 18;
+
+               if(m1 > n)
+               {
+                  _asm nop;
+                  break;
+               }
+
+               while(m1 < ((int)n-0x1000))
+                  m1 += 0x1000;
+
+               while(m2-- && n<length)
+                  buffer[n++] = buffer[m1++];
+            }
+            if(n>=length)
+               break;
+         }
+      }
+
+      unsigned char* bufptr = buffer;
+
+      // decode conv. header
+
+      unsigned int unknown1 = *(unsigned int*)bufptr;
+      bufptr+= 4;
+      unsigned int codesize = *(unsigned int*)bufptr;
+      bufptr+= 4;
+
+      unsigned short unknown2 = *(unsigned short*)bufptr;
+      bufptr+= 2;
+      unsigned short strblock = *(unsigned short*)bufptr;
+      bufptr+= 2;
+      unsigned short initstack = *(unsigned short*)bufptr;
+      bufptr+= 2;
+
+      // output conversation info
+      fprintf(dest,"; conversation #%u: offset=0x%04x, unk1=0x%04x, codesize=0x%04x\n",
+         i, offsets[i],unknown1,codesize,length);
+
+      fprintf(dest,"; unk2=%04x, conv=%04x, init_stack=%04x\n",
+         unknown2,strblock,initstack);
+
+      unsigned short globcount = *(unsigned short*)bufptr;
+      bufptr+= 2;
+
+      for(int a=0; a<globcount; a++)
+      {
+         unsigned short namelen = *(unsigned short*)bufptr;
+         bufptr+= 2;
+
+         std::string funcname;
+         funcname.append((const char*)bufptr,namelen);
+
+         bufptr += namelen;
+
+         unsigned short func_id = *(unsigned short*)bufptr;
+         bufptr+= 2;
+         unsigned short unk1 = *(unsigned short*)bufptr;
+         bufptr+= 2;
+         unsigned short import_type = *(unsigned short*)bufptr;
+         bufptr+= 2;
+         unsigned short ret_type = *(unsigned short*)bufptr;
+         bufptr+= 2;
+
+         if (import_type == 0x010F)
+         {
+            fprintf(dest,"; var mem=%04x (ret_type=%04x, unk1=%04x) name=\"%s\" \n",
+               func_id,ret_type,unk1,funcname.c_str());
+         }
+         else
+         if (import_type == 0x0111)
+         {
+            fprintf(dest,"; func id=%04x ret_type=%-6s (unk1=%04x) name=\"%s\"\n",
+               func_id,
+               ret_type==0 ? "void" : ret_type==0x0129? "int" :
+                  ret_type==0x012b ? "string" : "unknown",
+               unk1,funcname.c_str());
+         }
+         else
+            fprintf(dest,"; unknown import type %04x for name=\"%s\"\n",
+               import_type,funcname.c_str());
+      }
+
+      // get pointer to codes
+      unsigned short *allcodes = (unsigned short*)bufptr;
+
+      // decode them
+      decode_instructions(allcodes,dest,codesize);
+
+      delete buffer;
+#else
 
       unsigned int unknown1;
       fread(&unknown1,1,4,fd);
@@ -228,15 +346,26 @@ int main()
       fprintf(dest,"; unk2=%04x, conv=%04x, init_stack=%04x\n",
          unknown2[0],unknown2[1],unknown2[2]);
 
-      fprintf(dest,"; string block to use: %u\n",slotcount+25);
+      fprintf(dest,"; string block to use: %04x\n",i+0x0e00);
 
       dump_import_funcs(fd,dest);
-      decode_instructions(fd,dest,codesize);
 
+      // read in codes
+      unsigned short *allcodes = new unsigned short[codesize+1];
+      fread(allcodes,2,codesize,fd);
+
+      // decode them
+      decode_instructions(allcodes,dest,codesize);
+
+      delete[] allcodes;
+
+#endif
       fprintf(dest,"; -----------------------------------------\n\n");
 
-      slotcount++;
+      convcount++;
    }
+
+   fprintf(dest,"; dumped %u conversations\n\n",convcount);
 
    fclose(dest);
 
