502e8d3d7763402a7ddbb4c8980b39fc2f483542
vividos <>
2003-08-15 20:43:52 +0000

    added file menu items, a toolbar, support for document manager and a main frame icon

--- uwadv/source/debug/mainframe.cpp
+++ uwadv/source/debug/mainframe.cpp
@@ -35,6 +35,9 @@
 #include "tilemapview.hpp"
 #include "luasrcframe.hpp"
 
+// wxWindows includes
+#include "wx/filedlg.h"
+
 // frame layout library
 
 // extra plugins
@@ -52,9 +55,17 @@
 
 // ua_debugger_main_frame event table
 
-BEGIN_EVENT_TABLE(ua_debugger_main_frame, wxMDIParentFrame)
+BEGIN_EVENT_TABLE(ua_debugger_main_frame, wxDocMDIParentFrame)
+   EVT_MENU(MENU_FILE_NEW, ua_debugger_main_frame::OnMenuFileNew)
    EVT_MENU(MENU_FILE_OPEN, ua_debugger_main_frame::OnMenuFileOpen)
+   EVT_MENU(MENU_FILE_CLOSE, ua_debugger_main_frame::OnMenuFileClose)
+   EVT_MENU(MENU_FILE_SAVE, ua_debugger_main_frame::OnMenuFileSave)
+   EVT_MENU(MENU_FILE_SAVEAS, ua_debugger_main_frame::OnMenuFileSaveAs)
+   EVT_MENU(MENU_FILE_SAVEALL, ua_debugger_main_frame::OnMenuFileSaveAll)
    EVT_MENU(MENU_FILE_QUIT, ua_debugger_main_frame::OnMenuFileQuit)
+
+   EVT_UPDATE_UI(MENU_FILE_CLOSE, ua_debugger_main_frame::OnUpdateUIFileClose)
+
    EVT_MENU(MENU_UNDERW_UPDATE, ua_debugger_main_frame::OnMenuUnderwUpdate)
    EVT_MENU(MENU_UNDERW_SUSPEND, ua_debugger_main_frame::OnMenuUnderwSuspend)
    EVT_MENU(MENU_UNDERW_RESUME, ua_debugger_main_frame::OnMenuUnderwResume)
@@ -67,37 +78,79 @@ END_EVENT_TABLE()
 
 // ua_debugger_main_frame methods
 
-ua_debugger_main_frame::ua_debugger_main_frame(wxWindow* parent,
+ua_debugger_main_frame::ua_debugger_main_frame(
+   wxDocManager* doc_manager, wxFrame* parent,
    const wxWindowID id, const wxString& title,
    const wxPoint& pos, const wxSize& size, const long style)
-:wxMDIParentFrame(parent, id, title, pos, size, style)
+:wxDocMDIParentFrame(doc_manager, parent, id, title, pos, size, style)
 {
    // frame layout manager
-   m_pLayout = new wxFrameLayout(this, GetClientWindow());
+   {
+      m_pLayout = new wxFrameLayout(this, GetClientWindow());
 
-   m_pLayout->SetUpdatesManager(new cbGCUpdatesMgr());
+      m_pLayout->SetUpdatesManager(new cbGCUpdatesMgr());
 
-   // setup plugins
-   m_pLayout->PushDefaultPlugins();
+      // setup plugins
+      m_pLayout->PushDefaultPlugins();
 
-   m_pLayout->AddPlugin( CLASSINFO(cbBarHintsPlugin) );
-   m_pLayout->AddPlugin( CLASSINFO(cbHintAnimationPlugin) );
-//   m_pLayout->AddPlugin( CLASSINFO(cbRowDragPlugin) ); // mozilla-like bars
-   m_pLayout->AddPlugin( CLASSINFO(cbAntiflickerPlugin) );
-   m_pLayout->AddPlugin( CLASSINFO(cbSimpleCustomizationPlugin) );
+      m_pLayout->AddPlugin( CLASSINFO(cbBarHintsPlugin) );
+      m_pLayout->AddPlugin( CLASSINFO(cbHintAnimationPlugin) );
+   //   m_pLayout->AddPlugin( CLASSINFO(cbRowDragPlugin) ); // mozilla-like bars
+      m_pLayout->AddPlugin( CLASSINFO(cbAntiflickerPlugin) );
+      m_pLayout->AddPlugin( CLASSINFO(cbSimpleCustomizationPlugin) );
 
-   m_pLayout->EnableFloating(TRUE);
+      m_pLayout->EnableFloating(TRUE);
+   }
 
 
-   // menu stuff
+   // docview stuff
+   {
+      new wxDocTemplate(doc_manager,
+         "lua source file", "*.lua", "", "lua", "lua source file", "loa source file view",
+          CLASSINFO(ua_lua_document), CLASSINFO(ua_lua_view));
+         /*
+   dir_view_templ = new d64view_doc_template((wxDocManager*)doc_manager,
+      "d64 disk image", "*.d64", "", "d64", "d64 disk image", "d64 disk image directory view",
+          CLASSINFO(disk_document), CLASSINFO(dir_view));
+*/
+   }
+
+
+   // application icon
+#ifdef __WXMSW__
+   SetIcon(wxIcon("uadebug"));
+#endif
+
+
+   // frame menu
    menubar = new wxMenuBar();
    AddFrameMenus(menubar);
 
    SetMenuBar(menubar);
 
+
    // status bar
    CreateStatusBar(3);
    SetStatusText("ready.", 0);
+
+
+   // toolbar
+   {
+      wxToolBar* tb = CreateToolBar(wxTB_FLAT | wxTB_HORIZONTAL,-1,"toolbar");
+
+      wxBitmap toolBarBitmaps[3];
+
+      toolBarBitmaps[0] = wxBitmap("new");
+      toolBarBitmaps[1] = wxBitmap("open");
+      toolBarBitmaps[2] = wxBitmap("save");
+
+      tb->AddTool(MENU_FILE_NEW/*wxID_NEW*/, _T("New"),   toolBarBitmaps[0], _T("New file"));
+      tb->AddTool(MENU_FILE_OPEN/*wxID_OPEN*/, _T("Open"), toolBarBitmaps[1], _T("Open file"));
+      tb->AddTool(MENU_FILE_SAVE/*wxID_SAVE*/, _T("Save"), toolBarBitmaps[2], _T("Save file"), wxITEM_CHECK);
+
+      tb->Realize();
+   }
+
 
    // update all bars and windows
    UpdateAll();
@@ -107,7 +160,13 @@ void ua_debugger_main_frame::AddFrameMenus(wxMenuBar* menubar)
 {
    // file menu
    wxMenu* filemenu = new wxMenu();
-   filemenu->Append(MENU_FILE_OPEN, "&Open", "opens a Lua source file");
+   filemenu->Append(MENU_FILE_NEW, "&New\tCtrl+N", "creates an empty Lua source file");
+   filemenu->Append(MENU_FILE_OPEN, "&Open ...\tCtrl+O", "opens a Lua source file");
+   filemenu->Append(MENU_FILE_CLOSE, "&Close", "opens a Lua source file");
+   filemenu->AppendSeparator();
+   filemenu->Append(MENU_FILE_SAVE, "&Save\tCtrl+S", "saves the current Lua source file");
+   filemenu->Append(MENU_FILE_SAVEAS, "Save &As ...", "saves the current Lua source file under another name");
+   filemenu->Append(MENU_FILE_SAVEALL, "Save A&ll", "saves all open Lua source files");
    filemenu->AppendSeparator();
    filemenu->Append(MENU_FILE_QUIT, "&Quit", "quits the debugger");
    menubar->Append(filemenu, "&File");
@@ -166,15 +225,53 @@ bool ua_debugger_main_frame::CheckBarAvail(wxString& barname)
    return barinfo != NULL;
 }
 
+void ua_debugger_main_frame::OnMenuFileNew(wxCommandEvent& event)
+{
+   wxString filename("");
+   new ua_lua_source_frame(filename, this, -1, wxDefaultPosition,
+      wxDefaultSize, wxDEFAULT_FRAME_STYLE);
+}
+
 void ua_debugger_main_frame::OnMenuFileOpen(wxCommandEvent& event)
 {
-   new ua_lua_source_frame(this, -1, wxDefaultPosition, wxDefaultSize,
-      wxDEFAULT_FRAME_STYLE);
+   wxFileDialog filedlg(this,
+      "Please locate the Lua source file to open",
+      "", "", "Lua Source Files (*.lua)|*.lua");
+
+   filedlg.SetDirectory(wxGetHomeDir());
+
+   if (filedlg.ShowModal() == wxID_OK)
+   {
+      wxString filename(filedlg.GetPath());
+      new ua_lua_source_frame(filename, this, -1, wxDefaultPosition,
+         wxDefaultSize, wxDEFAULT_FRAME_STYLE);
+   }
+}
+
+void ua_debugger_main_frame::OnMenuFileClose(wxCommandEvent& event)
+{
+}
+
+void ua_debugger_main_frame::OnMenuFileSave(wxCommandEvent& event)
+{
+}
+
+void ua_debugger_main_frame::OnMenuFileSaveAs(wxCommandEvent& event)
+{
+}
+
+void ua_debugger_main_frame::OnMenuFileSaveAll(wxCommandEvent& event)
+{
 }
 
 void ua_debugger_main_frame::OnMenuFileQuit(wxCommandEvent& event)
 {
    Close(TRUE);
+}
+
+void ua_debugger_main_frame::OnUpdateUIFileClose(wxUpdateUIEvent& event)
+{
+   event.Enable(false);
 }
 
 void ua_debugger_main_frame::OnMenuUnderwUpdate(wxCommandEvent& event)
