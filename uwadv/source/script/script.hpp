62a18ce37ed74af8b0a45c5ac83b0f13e8a86ef3
vividos <>
2003-09-01 20:15:19 +0000

    added scripting base include file

---
+++ uwadv/source/script/script.hpp
@@ -0,0 +1,44 @@
+/*
+   Underworld Adventures - an Ultima Underworld hacking project
+   Copyright (c) 2002,2003 Underworld Adventures Team
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+   $Id$
+
+*/
+/*! \file script.hpp
+
+   \brief scripting support for underworld adventures
+
+*/
+
+// include guard
+#ifndef uwadv_script_hpp_
+#define uwadv_script_hpp_
+
+// needed includes
+
+
+// classes
+
+class ua_scripting
+{
+public:
+   //! ctor
+   ua_scripting(){}
+};
+
+#endif
