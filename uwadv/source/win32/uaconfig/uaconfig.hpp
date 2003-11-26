b59e6fa04a08f288be35090bd7ef977f15ffc7a8
vividos <>
2003-11-26 21:37:51 +0000

    fixed headers and include guard

--- uwadv/source/win32/uaconfig/uaconfig.hpp
+++ uwadv/source/win32/uaconfig/uaconfig.hpp
@@ -1,6 +1,6 @@
 /*
    Underworld Adventures - an Ultima Underworld hacking project
-   Copyright (c) 2002 Michael Fink
+   Copyright (c) 2002,2003 Underworld Adventures Team
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -25,8 +25,8 @@
 */
 
 // include guard
-#ifndef __uwadv_uaconfig_hpp_
-#define __uwadv_uaconfig_hpp_
+#ifndef uwadv_uaconfig_hpp_
+#define uwadv_uaconfig_hpp_
 
 // needed includes
 #include "common.hpp"
