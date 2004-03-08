dbbf1f12ed74ced3adf2004a8917440d512a5278
vividos <>
2004-03-08 20:26:49 +0000

    minor addition

--- uwadv/source/import/playerimport.cpp
+++ uwadv/source/import/playerimport.cpp
@@ -1,6 +1,6 @@
 /*
    Underworld Adventures - an Ultima Underworld hacking project
-   Copyright (c) 2002,2003 Underworld Adventures Team
+   Copyright (c) 2002,2003,2004 Underworld Adventures Team
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -33,6 +33,9 @@
 
 // ua_uw_import methods
 
+/*! \todo implement */
 void ua_uw_import::load_player(ua_player& player, const char* path)
 {
+
+   ua_runes& runes = player.get_runes();
 }
