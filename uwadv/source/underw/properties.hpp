f5ad8c4052cac737860a5101467115dcc83caa42
vividos <>
2004-03-15 19:41:23 +0000

    added is_container flag for common object properties added armour and wearable properties

--- uwadv/source/underw/properties.hpp
+++ uwadv/source/underw/properties.hpp
@@ -58,6 +58,9 @@ struct ua_common_obj_property
 
    //! indicates if object can be picked up
    bool can_be_picked_up;
+
+   //! indicates if object is a container
+   bool is_container;
 };
 
 //! info for melee weapons (ids 0x0000-0x000f)
@@ -73,10 +76,43 @@ struct ua_melee_weapon_property
    Uint8 durability;
 };
 
+//! info for ranged weapons (ids 0x0010-0x001f)
+struct ua_ranged_weapon_property
+{
+   //! weapon durability
+   Uint8 durability;
+};
+
+//! armour category
+enum ua_armour_category
+{
+   ua_armour_shield = 0,     //!< also: may not be worn on paperdoll
+   ua_armour_body_armour = 1,
+   ua_armour_leggings = 3,
+   ua_armour_gloves = 4,
+   ua_armour_boots = 5,
+   ua_armour_hat = 8,
+   ua_armour_ring = 9,
+};
+
+//! info for armour and wearables (ids 0x0020-0x003f)
+struct ua_armour_wearable_property
+{
+   //! protection value
+   Uint8 protection;
+
+   //! armour durability
+   Uint8 durability;
+
+   //! armour category (for paperdoll placement)
+   ua_armour_category category;
+};
+
 
 // classes
 
 //! object properties class
+/*! \todo implement loading all properties */
 class ua_object_properties
 {
 public:
@@ -89,8 +125,15 @@ public:
    //! returns common object properties about specific object
    inline ua_common_obj_property& get_common_property(Uint16 item_id);
 
+   //! returns properties about armour and wearables
+   inline ua_armour_wearable_property& get_armour_property(Uint16 item_id);
+
 protected:
+   //! common object properties
    std::vector<ua_common_obj_property> common_properties;
+
+   //! armour and wearables properties
+   std::vector<ua_armour_wearable_property> armour_wearable_properties;
 
    friend class ua_uw_import;
 };
@@ -105,6 +148,17 @@ inline ua_common_obj_property& ua_object_properties::get_common_property(
       item_id = common_properties.size()-1;
 
    return common_properties[item_id];
+}
+
+inline ua_armour_wearable_property& ua_object_properties::get_armour_property(
+   Uint16 item_id)
+{
+   item_id-=0x0020;
+
+   if (item_id+0x0020 >= common_properties.size())
+      item_id = common_properties.size()-1;
+
+   return armour_wearable_properties[item_id-0x0020];
 }
 
 
