ca3ced867b9d631e647503423e48149dc85b6d96
vividos <>
2004-03-24 22:52:32 +0000

    added more user_action() switch cases removed object_* and inventory_* methods added registering Lua C functions in tables, like ui.print()

--- uwadv/source/script/luascript.cpp
+++ uwadv/source/script/luascript.cpp
@@ -32,6 +32,11 @@
 #include "files.hpp"
 
 
+// constants
+
+const char* ua_lua_scripting::self_name = "_scripting_self";
+
+
 // ua_lua_scripting methods
 
 void ua_lua_scripting::init(ua_game_interface* the_game)
@@ -46,7 +51,11 @@ void ua_lua_scripting::init(ua_game_interface* the_game)
    lua_strlibopen(L);
    lua_mathlibopen(L);
 
-//   register_functions();
+   // set "self" pointer userdata
+   lua_pushuserdata(L,this);
+   lua_setglobal(L,self_name);
+
+   register_functions();
 }
 
 bool ua_lua_scripting::load_script(const char* basename)
@@ -129,6 +138,54 @@ void ua_lua_scripting::user_action(ua_underworld_user_action action,
 {
    switch(action)
    {
+   case ua_action_look_object:
+      lua_getglobal(L,"object_look");
+      lua_pushnumber(L, static_cast<double>(param));
+      checked_call(1,0);
+      break;
+
+   case ua_action_look_object_inventory:
+      lua_getglobal(L,"inventory_look");
+      lua_pushnumber(L, static_cast<double>(param));
+      checked_call(1,0);
+      break;
+
+   case ua_action_look_wall:
+      lua_getglobal(L,"wall_look");
+      lua_pushnumber(L, static_cast<double>(param));
+      checked_call(1,0);
+      break;
+
+   case ua_action_use_object:
+      lua_getglobal(L,"object_use");
+      lua_pushnumber(L, static_cast<double>(param));
+      checked_call(1,0);
+      break;
+
+   case ua_action_use_object_inventory:
+      lua_getglobal(L,"inventory_use");
+      lua_pushnumber(L, static_cast<double>(param));
+      checked_call(1,0);
+      break;
+
+   case ua_action_use_wall:
+      lua_getglobal(L,"wall_use");
+      lua_pushnumber(L, static_cast<double>(param));
+      checked_call(1,0);
+      break;
+
+   case ua_action_get_object:
+      lua_getglobal(L,"object_get");
+      lua_pushnumber(L, static_cast<double>(param));
+      checked_call(1,0);
+      break;
+
+   case ua_action_talk_object:
+      lua_getglobal(L,"object_talk");
+      lua_pushnumber(L, static_cast<double>(param));
+      checked_call(1,0);
+      break;
+
    case ua_action_clicked_spells:
       lua_getglobal(L,"spell_cast");
       checked_call(0,0);
@@ -141,19 +198,39 @@ void ua_lua_scripting::user_action(ua_underworld_user_action action,
       break;
 
    case ua_action_clicked_compass:
+      lua_getglobal(L,"ui_clicked_compass");
+      lua_pushnumber(L, static_cast<double>(param));
+      checked_call(1,0);
       break;
 
    case ua_action_clicked_vitality_flask:
+      lua_getglobal(L,"ui_clicked_vitality_flask");
+      lua_pushnumber(L, static_cast<double>(param));
+      checked_call(1,0);
       break;
 
    case ua_action_clicked_mana_flask:
+      lua_getglobal(L,"ui_clicked_mana_flask");
+      lua_pushnumber(L, static_cast<double>(param));
+      checked_call(1,0);
       break;
 
    case ua_action_clicked_gargoyle:
+      lua_getglobal(L,"ui_clicked_gargoyle");
+      lua_pushnumber(L, static_cast<double>(param));
+      checked_call(1,0);
       break;
 
    case ua_action_clicked_dragons:
+      lua_getglobal(L,"ui_clicked_dragons");
+      lua_pushnumber(L, static_cast<double>(param));
+      checked_call(1,0);
       break;
+
+      // ua_action_combat_enter
+      // ua_action_combat_draw
+      // ua_action_combat_leave
+      // ua_action_combat_release
 
    case ua_action_track_creatures:
       lua_getglobal(L,"track_creatures");
@@ -164,6 +241,12 @@ void ua_lua_scripting::user_action(ua_underworld_user_action action,
       lua_getglobal(L,"sleep");
       checked_call(0,0);
       break;
+
+   case ua_action_target_selected:
+      break;
+
+   default:
+      break;
    }
 }
 
@@ -171,34 +254,6 @@ void ua_lua_scripting::on_changing_level()
 {
    lua_getglobal(L,"on_change_level");
    checked_call(0,0);
-}
-
-void ua_lua_scripting::object_look(unsigned int pos)
-{
-   lua_getglobal(L,"object_look");
-   lua_pushnumber(L, static_cast<double>(pos));
-   checked_call(1,0);
-}
-
-void ua_lua_scripting::object_use(unsigned int pos)
-{
-   lua_getglobal(L,"object_look");
-   lua_pushnumber(L, static_cast<double>(pos));
-   checked_call(1,0);
-}
-
-void ua_lua_scripting::inventory_look(unsigned int pos)
-{
-   lua_getglobal(L,"inventory_look");
-   lua_pushnumber(L, static_cast<double>(pos));
-   checked_call(1,0);
-}
-
-void ua_lua_scripting::inventory_use(unsigned int pos)
-{
-   lua_getglobal(L,"inventory_use");
-   lua_pushnumber(L, static_cast<double>(pos));
-   checked_call(1,0);
 }
 
 ua_item_combine_status ua_lua_scripting::item_combine(Uint16 item_id1,
@@ -241,4 +296,49 @@ int ua_lua_scripting::load_script(SDL_RWops* rwops, const char* chunkname)
 ua_scripting* ua_scripting::create_scripting(ua_scripting_language)
 {
    return new ua_lua_scripting;
+}
+
+ua_lua_scripting& ua_lua_scripting::get_scripting_from_self(lua_State* L)
+{
+   ua_lua_scripting* self = NULL;
+
+   lua_getglobal(L,self_name);
+   if (!lua_isuserdata(L,-1))
+      throw ua_exception("'self' parameter wasn't set by ua_lua_scripting");
+
+   // get pointer to underworld
+   self = reinterpret_cast<ua_lua_scripting*>(lua_touserdata(L,-1));
+
+   if (self == NULL || self->L != L)
+      throw ua_exception("wrong 'self' parameter in ua_lua_scripting");
+
+   lua_pop(L,1);
+
+   return *self;
+}
+
+#define lua_ex_register_table(L, n, f) { \
+   lua_pushstring(L, n); lua_pushcfunction(L, f); lua_settable(L,-3); }
+
+void ua_lua_scripting::register_functions()
+{
+   // ui object
+   lua_newtable(L);
+
+   lua_ex_register_table(L, "print", ui_print);
+
+   lua_setglobal(L, "ui");
+}
+
+int ua_lua_scripting::ui_print(lua_State* L)
+{
+   ua_lua_scripting& self = get_scripting_from_self(L);
+
+   ua_underworld_callback* callback = self.game->get_underworld().get_callback();
+   if (callback != NULL)
+   {
+      const char* text = lua_tostring(L,-1);
+      callback->uw_print(text);
+   }
+   return 0;
 }
