f97eb5c8d701b19ada9b673b1670d110ce4e2b13
vividos <>
2004-05-17 19:22:02 +0000

    added conversation code loader

--- uwadv/source/import/convloader.cpp
+++ uwadv/source/import/convloader.cpp
@@ -29,7 +29,7 @@
 #include "common.hpp"
 #include "import.hpp"
 #include "io_endian.hpp"
-#include "conv/codevm.hpp"
+#include "codevm.hpp"
 
 
 //! Loads conversation globals from bglobals.dat or babglobs.dat
@@ -78,4 +78,138 @@ void ua_uw_import::load_conv_globals(ua_conv_globals& globals,
    }
 
    fclose(fd);
+}
+
+
+// ua_conv_vm methods
+
+/*! Loads conversation code from given file, into the virtual machine.
+    It loads all imported symbols (functions and globals) and puts the opcodes
+    into the vm's code segment. The method ua_conv_code_vm::init() can then be
+    called after this method.
+
+    \param vm conversation code virtual machine
+    \param cnvfile conversation filename, e.g. cnv.ark
+    \param conv convesation slot of code to load
+*/
+bool ua_uw_import::load_conv_code(ua_conv_code_vm& vm, const char* cnvfile,
+   Uint16 conv)
+{
+   FILE *fd = fopen(cnvfile,"rb");
+   if (fd==NULL)
+      throw ua_exception("could not open conversation file");
+
+   // read number of entries
+   Uint16 entries = fread16(fd);
+
+   if (conv>entries)
+   {
+      fclose(fd);
+      throw ua_exception("invalid conversation!");
+   }
+
+   fseek(fd,conv*4+2,SEEK_SET);
+
+   Uint32 offset = fread32(fd);
+
+   if (offset==0)
+   {
+      // no conversation available
+      fclose(fd);
+      return false;
+   }
+
+   // read conversation header
+   fseek(fd,offset,SEEK_SET);
+
+   Uint32 unk1 = fread32(fd); // always 0x00000828
+
+   Uint16 codesize = fread16(fd);
+   Uint16 unk2 = fread16(fd); // always 0x0000
+   Uint16 unk3 = fread16(fd); // always 0x0000
+
+   Uint16 strblock = fread16(fd); // string block to use
+   vm.set_strblock(strblock);
+
+   Uint16 glob_reserved = fread16(fd); // number of stack words reserved for globals
+   vm.set_globals_reserved(glob_reserved);
+
+   // load imported functions
+   load_conv_code_imported_funcs(vm, fd);
+
+   // load code
+   std::vector<Uint16>& code = vm.get_code_segment();
+
+   code.resize(codesize,0);
+   for(Uint16 i=0; i<codesize; i++)
+      code[i] = fread16(fd);
+
+   fclose(fd);
+
+   vm.set_conv_slot(conv);
+
+   return true;
+}
+
+void ua_uw_import::load_conv_code_imported_funcs(ua_conv_code_vm& vm,
+   FILE *fd)
+{
+   std::map<Uint16,ua_conv_imported_item>& imported_globals = vm.get_imported_funcs();
+   std::map<Uint16,ua_conv_imported_item>& imported_funcs = vm.get_imported_globals();
+
+   imported_funcs.clear();
+   imported_globals.clear();
+
+   // read number of imported funcs
+   Uint16 funcs = fread16(fd);
+
+   for(int i=0; i<funcs; i++)
+   {
+      // length of function name
+      Uint16 fname_len = fread16(fd);
+      if (fname_len>255) fname_len = 255;
+
+      char funcname[256];
+      fread(funcname,1,fname_len,fd);
+      funcname[fname_len]=0;
+
+      // function ID
+      Uint16 func_id = fread16(fd);
+      Uint16 fn_unknown1 = fread16(fd); // always seems to be 1
+      Uint16 import_type = fread16(fd);
+      Uint16 ret_type = fread16(fd);
+
+      // fill imported item struct
+      ua_conv_imported_item iitem;
+
+      // determine return type
+      switch(ret_type)
+      {
+      case 0x0000: iitem.datatype = ua_dt_void; break;
+      case 0x0129: iitem.datatype = ua_dt_int; break;
+      case 0x012b: iitem.datatype = ua_dt_string; break;
+      default:
+         throw ua_exception("unknown return type in conv imports list");
+      }
+
+      iitem.name = funcname;
+
+      // store imported item in appropriate list
+      switch(import_type)
+      {
+      case 0x0111:
+         // imported function
+         imported_funcs[func_id] = iitem;
+         break;
+
+      case 0x010F:
+         // imported global
+         imported_globals[func_id] = iitem;
+         break;
+
+      default:
+         throw ua_exception("unknown import type in conv imports list");
+         break;
+      }
+   }
 }
