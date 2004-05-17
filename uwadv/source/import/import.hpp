af0e20dfedcfe2df8808269a8ac2d990cbcb5de1
vividos <>
2004-05-17 19:20:41 +0000

    moved graphics import functions to importgfx.hpp and own classes; import.hpp now only contains ua_underworld objects import functions made ua_uw2_import::get_rwops_uw2dec() public and static to be useful in other code parts added conversation code functions

--- uwadv/source/import/import.hpp
+++ uwadv/source/import/import.hpp
@@ -1,6 +1,6 @@
 /*
    Underworld Adventures - an Ultima Underworld hacking project
-   Copyright (c) 2003 Underworld Adventures Team
+   Copyright (c) 2003,2004 Underworld Adventures Team
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -23,16 +23,23 @@
 
    \brief game data import
 
-   import
+*/
+/*! \defgroup import Import
+
+   import documentation yet to come ...
 
 */
+//@{
+
 
 // include guard
 #ifndef uwadv_import_hpp_
 #define uwadv_import_hpp_
 
 // needed includes
-#include "image.hpp"
+#include <string>
+#include <vector>
+#include <map>
 
 
 // forward references
@@ -43,11 +50,8 @@ class ua_object_list;
 class ua_object;
 class ua_object_properties;
 class ua_conv_globals;
+class ua_conv_code_vm;
 class ua_settings;
-class ua_font;
-class ua_image;
-struct ua_lp_descriptor;
-class ua_critter;
 
 
 //! imports common to uw1 and uw2
@@ -76,55 +80,10 @@ public:
    void load_conv_globals(ua_conv_globals& globals, ua_settings& settings,
       const char* folder, bool initial);
 
-
-   // visuals loading
-
-   //! loads 8 main palettes
-   void load_palettes(const char* allpalname,
-      ua_palette256_ptr allpalettes[8]);
-
-   //! loads all 32 auxiliary palettes with 16 indices each
-   void load_aux_palettes(const char* auxpalname,
-      Uint8 allauxpals[32][16]);
-
-   //! loads a *.gr image
-   void load_image_gr(ua_image& img, const char* imgname, unsigned int imgnum,
-      Uint8 auxpalettes[32][16]);
-
-   //! loads a list of image from a *.gr file
-   void load_image_gr_list(std::vector<ua_image>& imglist,
-      const char* imgname, unsigned int img_from, unsigned int img_to,
-      Uint8 auxpalettes[32][16]);
-
-   //! loads a *.byt image
-   void load_image_byt(const char* imgname, Uint8* pixels);
-
-   //! loads texture images
-   void load_textures(std::vector<ua_image>& tex_images,
-      unsigned int startidx, const char* texname,
-      ua_palette256_ptr palette);
-
-   //! loads a font
-   void load_font(const char* fontname, ua_font& font);
-
-   //! loads cutscene data
-   void load_cutscene(const char* filename, ua_image& image,
-      std::vector<ua_lp_descriptor>& lpdarray,
-      std::vector<Uint8>& lpages, unsigned int& records);
-
-   //! extracts cutscene data from source data
-   void extract_cutscene_data(Uint8* src, Uint8* dst, unsigned int maxpix);
-
-   //! loads all critters
-   void load_critters(std::vector<ua_critter>& allcritters,
-      ua_settings& settings, ua_palette256_ptr palette0);
-
-   //! loads all frames of a critter
-   void load_critter(ua_critter& critter, const char* file,
-      unsigned int used_auxpal);
+   //! loads conversation code into virtual machine
+   bool load_conv_code(ua_conv_code_vm& vm, const char* cnvfile, Uint16 conv);
 
 protected:
-
    //! loads tilemap infos
    void load_tilemap(ua_level& level, SDL_RWops* rwops, Uint16 textures[64], bool uw2_mode);
 
@@ -136,10 +95,8 @@ protected:
       Uint32 objprop[0x400*2], Uint8 npcinfo[0x100*19],
       Uint16 objpos, Uint16 texmap[64], Uint8 tilex, Uint8 tiley);
 
-private:
-   //! loads *.gr image into pixels array
-   void load_image_gr_impl(ua_image& image, FILE* fd, Uint8 auxpalidx[32][16],
-      bool special_panels);
+   //! loads imported functions list
+   void load_conv_code_imported_funcs(ua_conv_code_vm& vm, FILE *fd);
 };
 
 //! uw1 specific imports
@@ -149,10 +106,6 @@ public:
    //! loads uw1 or uw_demo level maps
    void load_levelmaps(std::vector<ua_level> &levels,
       ua_settings &settings, const char* folder);
-
-   //! loads all uw1 critters
-   void load_critters(std::vector<ua_critter>& allcritters,
-      ua_settings& settings, ua_palette256_ptr palette0);
 };
 
 //! uw2 specific imports
@@ -163,14 +116,11 @@ public:
    void load_levelmaps(std::vector<ua_level> &levels,
       ua_settings &settings, const char* folder);
 
-   //! loads all uw2 critters
-   void load_critters(std::vector<ua_critter>& allcritters,
-      ua_settings& settings, ua_palette256_ptr palette0);
+   //! creates SDL_RWops struct from compressed .ark file blocks (uw2 only)
+   static SDL_RWops* get_rwops_uw2dec(FILE* fd,unsigned int blocknum,
+      unsigned int destsize);
 
 protected:
-   //! creates SDL_RWops struct from compressed .ark file blocks (uw2 only)
-   SDL_RWops* get_rwops_uw2dec(FILE* fd,unsigned int blocknum, unsigned int destsize);
-
    //! callback function to free uw2dec RWops memory
    static int uw2dec_close(SDL_RWops* rwops);
 };
@@ -236,3 +186,4 @@ inline Uint32 ua_get_bits(Uint32 value, unsigned int start, unsigned int count)
 
 
 #endif
+//@}
