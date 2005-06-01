f3d52b105d4b53be35836b94e1eedcc99a2bcd3e
vividos <>
2005-06-01 18:07:39 +0000

    added some documentation removed some whitespaces

--- uwadv/source/audio/midi_driver/win_midiout.h
+++ uwadv/source/audio/midi_driver/win_midiout.h
@@ -15,6 +15,14 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
+/*! \file win_midiout.h
+
+   \brief midi driver for Windows
+
+*/
+//! \ingroup audio
+
+//@{
 
 #ifndef WIN_MIDIOUT_H
 #define WIN_MIDIOUT_H
@@ -45,18 +53,20 @@
 
 #include "../midi.hpp"
 
-class	ua_win_midiout : virtual public ua_midi_driver
+class ua_win_midiout : virtual public ua_midi_driver
 {
 public:
-	virtual void start_track(XMIDIEventList *, bool repeat);
-	virtual void start_sfx(XMIDIEventList *);
-	virtual void stop_track(void);
-	virtual void stop_sfx(void);
-	virtual bool is_playing(void);
-	virtual const char *copyright(void);
+   virtual void start_track(XMIDIEventList*, bool repeat);
+   virtual void start_sfx(XMIDIEventList*);
+   virtual void stop_track(void);
+   virtual void stop_sfx(void);
+   virtual bool is_playing(void);
+   virtual const char* copyright(void);
 
-	ua_win_midiout();
-	virtual ~ua_win_midiout();
+   //! ctor
+   ua_win_midiout();
+   //! dtor
+   virtual ~ua_win_midiout();
 
    //! initializes driver
    virtual bool init_driver();
@@ -65,75 +75,76 @@ public:
    void set_dev_num(signed int num){ dev_num = num; }
 
 private:
+   struct mid_data {
+      XMIDIEventList* list;
+      bool repeat;
+   };
 
-	struct mid_data {
-		XMIDIEventList	*list;
-		bool			repeat;
-	};
+   static const unsigned short  centre_value;
+   static const unsigned char   fine_value;
+   static const unsigned char   coarse_value;
+   static const unsigned short  combined_value;
 
-	static const unsigned short	centre_value;
-	static const unsigned char	fine_value;
-	static const unsigned char	coarse_value;
-	static const unsigned short	combined_value;
+   signed int  dev_num;
+   HMIDIOUT    midi_port;
+   
+   HANDLE*     thread_handle;
+   DWORD       thread_id;
 
-	signed int	dev_num;
-	HMIDIOUT	midi_port;
-	
-	HANDLE	 	*thread_handle;
-	DWORD		thread_id;
+   // Thread communicatoins
+   LONG      is_available;
+   LONG      playing;
+   LONG      s_playing;
+   LONG      thread_com;
+   LONG      sfx_com;
 
-	// Thread communicatoins
-	LONG		is_available;
-	LONG		playing;
-	LONG		s_playing;
-	LONG		thread_com;
-	LONG		sfx_com;
+   mid_data* thread_data;
+   mid_data* sfx_data;
 
-	mid_data *thread_data;
-	mid_data *sfx_data;
+   mid_data data;
+   mid_data sdata;
 
-	mid_data data;
-	mid_data sdata;
+   // Methods
+   static DWORD __stdcall thread_start(void *data);
+   void init_device();
+   DWORD thread_main();
+   void thread_play ();
+   void reset_channel (int i);
 
-	// Methods
-	static DWORD __stdcall thread_start(void *data);
-	void init_device();
-	DWORD thread_main();
-	void thread_play ();
-	void reset_channel (int i);
+   // Microsecond Clock
+   unsigned long start;
+   unsigned long sfx_start;
 
-	// Microsecond Clock
-	unsigned long start;
-	unsigned long sfx_start;
+   inline void wmoInitClock ()
+   { start = GetTickCount()*6; }
 
-	inline void wmoInitClock ()
-	{ start = GetTickCount()*6; }
+   inline void wmoAddOffset (unsigned long offset)
+   { start += offset; }
 
-	inline void wmoAddOffset (unsigned long offset)
-	{ start += offset; }
+   inline unsigned long wmoGetTime ()
+   { return GetTickCount()*6 - start; }
 
-	inline unsigned long wmoGetTime ()
-	{ return GetTickCount()*6 - start; }
+   inline unsigned long wmoGetStart ()
+   { return start; }
 
-	inline unsigned long wmoGetStart ()
-	{ return start; }
+   inline unsigned long wmoGetRealTime ()
+   { return GetTickCount()*6; }
 
-	inline unsigned long wmoGetRealTime ()
-	{ return GetTickCount()*6; }
+   inline void wmoInitSFXClock ()
+   { sfx_start = GetTickCount()*6; }
 
-	inline void wmoInitSFXClock ()
-	{ sfx_start = GetTickCount()*6; }
+   inline void wmoAddSFXOffset (unsigned long offset)
+   { sfx_start += offset; }
 
-	inline void wmoAddSFXOffset (unsigned long offset)
-	{ sfx_start += offset; }
+   inline unsigned long wmoGetSFXTime ()
+   { return GetTickCount()*6 - sfx_start; }
 
-	inline unsigned long wmoGetSFXTime ()
-	{ return GetTickCount()*6 - sfx_start; }
-
-	inline unsigned long wmoGetSFXStart ()
-	{ return sfx_start; }
+   inline unsigned long wmoGetSFXStart ()
+   { return sfx_start; }
 };
 
 #endif //WIN32
 
 #endif //WIN_MIDIOUT_H
+
+//@}
