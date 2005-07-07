88b2e886b2e364c3b55968331305f3121ee6fc55
vividos <>
2005-07-07 22:12:04 +0000

    reorganized toolbars added pause button

--- uwadv/source/win32/uadebug/resource.h
+++ uwadv/source/win32/uadebug/resource.h
@@ -10,6 +10,9 @@
 #define IDR_GAME_STRINGS                130
 #define IDR_TILEMAP_VIEW                131
 #define IDC_CURSOR_TILEMAP              200
+#define IDR_TOOLBAR_MAIN                201
+#define IDR_TOOLBAR_STANDARD            202
+#define IDR_TOOLBAR_DEBUG               203
 #define IDC_EDIT_TILEPOS                1000
 #define IDC_TILEMAP                     1001
 #define IDC_LIST_STRINGS                1002
@@ -24,7 +27,7 @@
 #define ID_FILE_SAVE_ALL                32776
 #define ID_FILE_OPEN_PROJECT            32777
 #define ID_FILE_CLOSE_PROJECT           32778
-#define ID_UNDERWORLD_RUNNING           32780
+#define ID_UNDERWORLD_RUN               32780
 #define ID_VIEW_TILEMAPVIEW             32781
 #define ID_VIEW_TILEMAP                 32782
 #define ID_VIEW_GAMESTRINGS             32788
@@ -34,13 +37,27 @@
 #define ID_TILEMAP_ZOOMIN               32794
 #define ID_TILEMAP_ZOOMOUT              32795
 #define ID_VIEW_TILEINFO                32797
+#define ID_DEBUG_STEP_INTO              32801
+#define ID_DEBUG_STEP_OVER              32802
+#define ID_DEBUG_STEP_OUT               32803
+#define ID_DEBUG_RUN                    32804
+#define ID_GAME_NEW                     32806
+#define ID_VIEW_OUTPUT                  32808
+#define ID_VIEW_TOOLBAR_DEBUG           32811
+#define ID_VIEW_TOOLBAR_STANDARD        32812
+#define ID_FILE_SAVEALL                 32816
+#define ID_BUTTON32820                  32820
+#define ID_BUTTON32821                  32821
+#define ID_BUTTON32822                  32822
+#define ID_BUTTON32823                  32823
+#define ID_UNDERWORLD_PAUSE             32824
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        203
-#define _APS_NEXT_COMMAND_VALUE         32801
+#define _APS_NEXT_RESOURCE_VALUE        204
+#define _APS_NEXT_COMMAND_VALUE         32825
 #define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           103
 #endif
