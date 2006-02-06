e5316313bf5261bf5e85bd0652cad4610979494e
vividos <>
2006-02-06 21:10:14 +0000

    glue code to get Pentagram sourcecode to work with uwadv

---
+++ uwadv/source/audio/midi/common_types.h
@@ -0,0 +1,66 @@
+/*
+   Underworld Adventures - an Ultima Underworld remake project
+   Copyright (c) 2006 Michael Fink
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+   $Id$
+
+*/
+/*! \file common_types.h
+
+   \brief needed typedefs for Pentagram
+
+*/
+
+#ifndef uwadv_audio_midi_common_types_h_
+#define uwadv_audio_midi_common_types_h_
+
+#include <SDL_types.h>
+
+// derive Pentagram types from SDL types
+typedef Uint32 uint32;
+typedef Uint16 uint16;
+typedef Uint8 uint8;
+
+typedef Sint32 sint32;
+typedef Sint16 sint16;
+typedef Sint8 sint8;
+
+/*! \todo mapping is static for now; fails on 64-bit targets; remove uintptr
+    instead
+*/
+#define SIZEOF_INT 4
+#define SIZEOF_INTP 4
+#define SIZEOF_LONG 4
+#define SIZEOF_LONG_LONG 8
+
+
+#ifndef EX_TYPE_INTPTR
+#  if (SIZEOF_INTP == SIZEOF_INT)
+#    define EX_TYPE_INTPTR int
+#  elif (SIZEOF_INTP == SIZEOF_LONG)
+#    define EX_TYPE_INTPTR long
+#  elif (SIZEOF_INTP == SIZEOF_LONG_LONG)
+#    define EX_TYPE_INTPTR long long
+#  else
+#    error "Size of pointer type not equal to int, long or long long"
+#  endif
+#endif
+
+typedef unsigned EX_TYPE_INTPTR uintptr;
+typedef signed EX_TYPE_INTPTR sintptr;
+
+#endif
---
+++ uwadv/source/audio/midi/pent_include.h
@@ -0,0 +1,76 @@
+/*
+   Underworld Adventures - an Ultima Underworld remake project
+   Copyright (c) 2006 Michael Fink
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+   $Id$
+
+*/
+/*! \file pent_include.h
+
+   \brief needed includes and stuff for Pentagram
+
+*/
+
+// include guard
+#ifndef uwadv_audio_midi_pent_include_h_
+#define uwadv_audio_midi_pent_include_h_
+
+// needed includes
+#include "base.hpp"
+
+#ifdef HAVE_MSVC
+#pragma warning(disable: 4100) // 'T' : unreferenced formal parameter
+#pragma warning(disable: 4244) // 'initializing' : conversion from 'T' to 'U', possible loss of data
+#pragma warning(disable: 4512) // 'T' : assignment operator could not be generated
+#pragma warning(disable: 4189) // 'T' : local variable is initialized but not referenced
+#pragma warning(disable: 4127) // conditional expression is constant
+#pragma warning(disable: 4201) // nonstandard extension used : nameless struct/union
+#pragma warning(disable: 4706) // assignment within conditional expression
+#endif
+
+#if defined(HAVE_WIN32) && !defined(WIN32)
+#define WIN32
+#endif
+
+#include "common_types.h"
+#include <string>
+#include <iostream>
+#include <cassert>
+
+
+#define pout std::cout
+#define perr std::cerr
+
+#define FORGET_OBJECT(x) (x = NULL)
+
+
+#include "string.hpp"
+
+namespace Pentagram
+{
+   inline int strcasecmp(const char* str1, const char* str2)
+   {
+      std::string strText1(str1), strText2(str2);
+      Base::String::Lowercase(strText1);
+      Base::String::Lowercase(strText2);
+      return strText1.compare(strText2);
+   }
+}
+
+#include "IDataSource.h"
+
+#endif
