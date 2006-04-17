5bfcf8fde87318b6635dbd35f12b79dcecc5fc41
vividos <>
2006-04-17 16:22:41 +0000

    added news item with status update

--- webpages/news.php
+++ webpages/news.php
@@ -1,51 +1,32 @@
+<div>
 <h1>Underworld Adventures</h1>
 
 <p>
 Welcome to the Underworld Adventures home page. Underworld Adventures is a project to recreate Ultima Underworld 1 on modern operating systems (e.g. Win32, Linux or MacOS), using the original game files. It uses OpenGL for 3D graphics, the SDL library for platform specific tasks and Lua for scripting. It is developed as Open Source and is licensed under the GNU General Public License.
 </p>
 
-<h2>project news</h2>
+<h2>Project News</h2>
 
-<span class="newsHeading">2004-10-12 - Developer Log updated; RSS feed available</span>
+<div id="entry">
+  <div id="heading" class="newsHeading">2006-04-17 - Project status</div>
 
-<p class="newsBody">
-I updated the <a href="index.php?page=devlog">"devlog"</a> with some of the recent ongoings on the uwadv project. I also added some screenshots of the new Underworld Adventures Debugger I'm currently working on. And the Underworld Adventures Developer Log got a RSS 2.0 feed recently. So if you want to be up-to-date, just add the feed to your favourite aggregator. If there's a problem with the feed, just drop me a mail.
-</p>
+  <p class="newsBody">
+  As some ask about the status of Underworld Adventures and if it's abandoned, I thought I post a new project news item to give a status update of the project.
+  </p>
 
-<hr size="1" width="80%"/>
-
-<span class="newsHeading">2004-04-23 - debian package for uwadv 0.9 "mojito" released</span>
-
-<p class="newsBody">
-Today the debian package for the 0.9 version was released. The packages were prepared for Debian/testing (the package should work for unstable, too). Grab it on the <a href="index.php?page=download">download page</a>.
-</p>
-
-<hr size="1" width="80%"/>
-
-<span class="newsHeading">2004-04-10 - new release: uwadv 0.9 "mojito"</span>
-
-<p class="newsBody">
-I'm happy to announce that after *mumble,mumble* months, we have released a new version of Underworld Adventures!<sup>[1]</sup>. This release codenamed after the cuban national drink doesn't have new features added. In fact it misses some features that were present in 0.8, but the source code was heavily restructured and refactored. Source code and binaries for Windows and Linux are available on the <a href="index.php?page=download">download page</a>. From now on releases will be done more frequently. It is planned to have a release at least every 2 months.
-</p>
-
-<p class="newsBody">
-We also released a new version of the digital music soundtrack pack that contains the 2 new and unreleased tracks "Wanderer" and "Maps & Legends" from Martin Gircys-Shetty (QQtis). The package is also available on the <a href="index.php?page=download">download page</a>. Thanks Martin!
-</p>
-
-<p class="newsBody">
-The project always searches for people to help out coding, porting and testing. We especially search for someone who ports the code to Mac OS X. Also some artwork is needed for the splash screen and a new installer. Please see the <a href="index.php?page=dev">development</a> section for more information.
-</p>
-
-<p class="newsBody">
-We also wish the Underworld Adventures a happy 2nd birthday anniversary! Two years ago I got the mail from SourceForge that my project is approved. Although readers of the <a href="index.php?page=devlog">dev log</a> know that the idea of the project emerged earlier, it's nevertheless a good date to release a new version. So enjoy this release!
-</p>
-
-<p class="newsBody">
-<small>[1] blatantly stolen from <a href="http://www.xiph.org/positron/" target="_blank">positron</a>'s news announcement.</small>
-</p>
+  <p class="newsBody">
+  The current release is the 0.9 release from two years ago. There was some work in CVS until the end of 2005, including a Mac OS X port, but nothing releaseable. In the beginning of 2006 I decided to rewrite the game, using the algorithms and functions already in uwadv. Until now I've rewritten about 20% of the classes in uwadv. Additonally unit tests were added for every class in uwadv, to ensure code stability.
+  </p>
+  
+  <p class="newsBody">
+   I expect that I have something releaseable by the end of the year (2006, that is!), but until then there won't be any releases that can be tried out. For an alpha version of the game, try out the latest release: <a href="index.php?page=download">0.9 "mojito"</a>.
+  </p>
+</div>
 
 <hr size="1" width="80%"/>
 
 <p>
 Older news bits can be found in the <a href="index.php?page=archive">news archive</a>.
 </p>
+
+</div>
