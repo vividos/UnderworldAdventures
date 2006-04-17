58996dabae17eb528886139acbde51d09ae16e19
vividos <>
2006-04-17 16:27:34 +0000

    updated project member list

--- webpages/aboutus.php
+++ webpages/aboutus.php
@@ -6,27 +6,7 @@ Mail address:<?php insert_mail(); ?>
 </p>
 
 <p>
-<b><a href="http://sourceforge.net/users/phlask">Dirk Manders (phlask)</a></b> project developer, located in the Netherlands. He did the introduction cutscene and the character creation screen.
-</p>
-
-<p>
-<b><a href="http://sourceforge.net/users/wjpalenstijn">Willem Jan Palenstijn (wjp)</a></b> he lives in the Netherlands and studies Mathematics at the Leiden University. His main task is Linux porting. Willem Jan also has a <a href="http://www.math.leidenuniv.nl/~wpalenst/ultima/" target="_blank">Ultima Maps and Tables</a> page.
-</p>
-
-<p>
-<b><a href="http://sourceforge.net/users/aishafenton">Aisha Fenton</a></b>, lives in New Zealand and works for a local software company there. Aisha is working on the Mac-OS X port.
-</p>
-
-<p>
-<b><a href="http://sourceforge.net/users/hairyjim">Jim Cameron (hairyjim)</a></b> TSSHP <a href="http://tsshp.sourceforge.net/" target="_blank">(The System Shock Hack Project)</a> is also interested in decoding Ultima Underworld data structures and helps out with the <a href="http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/uwadv/uwadv/docs/uw-formats.txt?rev=HEAD&amp;content-type=text/vnd.viewcvs-markup" target="_blank">uw-formats.txt</a> file. Jim is located in the UK.
-</p>
-
-<p>
-<b><a href="http://www.peroxide.dk/" target="_blank">Kasper Fauerby (Telemachos)</a></b> from the <a href="http://www.peroxide.dk/era/" target="_blank">Era: a legend is reborn</a> (former Ultima 1 remake) project is a great fan of the Underworld series and helps out a bit with the collision detection code. He's located in Denmark.
-</p>
-
-<p>
-<b>Martin Gircys-Shetty (QQtis)</b> [mshetty <b>a t</b> wam <b>d o t</b> umd <b>d o t</b> edu] lives in Washington, DC, and studies history at the University of Maryland. He works on digitally rearranging the Underworld midi soundtrack using a software synth. He has a <a href="http://www.wam.umd.edu/~mshetty/music/" target="_blank">folder with recent arrangements</a> available.
+<b><a href="http://sourceforge.net/users/wjpalenstijn">Willem Jan Palenstijn (wjp)</a></b> lives in the Netherlands and studies Mathematics at the Leiden University. His main task is Linux porting. Willem Jan also has a <a href="http://www.math.leidenuniv.nl/~wpalenst/ultima/" target="_blank">Ultima Maps and Tables</a> page.
 </p>
 
 <p>
