88dc191579d9156ca7e79a71b5e20c3795953ba1
vividos <>
2007-01-07 15:08:04 +0000

    added iframe with current log file

--- webpages/irc.php
+++ webpages/irc.php
@@ -1,9 +1,16 @@
 <h2>#uwadv IRC channel</h2>
 
 <p>
-Underworld Adventures has an official IRC channel. Just connect to irc.freenode.net and join #uwadv. Most activity will probably be after 19:00 GMT, as most project members are from europe.
+Underworld Adventures has an official IRC channel. Just connect to irc.freenode.net and join #uwadv.
 </p>
 
 <p>
 Exultbot is responsible for the <a href="http://www.math.leidenuniv.nl/~wpalenst/uwadvlog.php" target="_blank">#uwadv channel logs</a>. It also understands some commands. Just say "?help" in the channel to get an overview of the commands.
 </p>
+
+<!-- h2>today's log</h2>
+
+<p>
+Here you can see today's logfile:
+<iframe width="95%" height="300" src="http://www.math.leidenuniv.nl/~wpalenst/uwadvlog.php"/>
+</p -->
