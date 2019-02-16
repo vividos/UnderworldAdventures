99a43831911b6f7815654f8e47259f7808c9c351
Michael Fink <michael.fink@asamnet.de>
2019-02-16 21:04:01 +0100

    moved uamath.hpp to source folder and moved triangle classes to triangle3d.hpp

--- uwadv/source/uamath.hpp
+++ uwadv/source/uamath.hpp
@@ -29,8 +29,7 @@
 */
 
 // include guard
-#ifndef uwadv_base_uamath_hpp_
-#define uwadv_base_uamath_hpp_
+#pragma once
 
 // needed includes
 #include <cmath>
@@ -209,49 +208,6 @@ public:
    ua_vector3d normal; //!< plane normal
    double equation_3;  //!< 3rd plane equation param
 };
-
-
-//! vertex in 3d space
-/*! Represents a vertex in 3d space, which can form a triangle. The vertex
-    additionally has texture coordinates associated with them. */
-struct ua_vertex3d
-{
-   //! struct ctor
-   ua_vertex3d():u(0.0),v(0.0){}
-
-   //! vertex position
-   ua_vector3d pos;
-
-   //! texture coordinates
-   double u, v;
-};
-
-
-//! textured triangle
-/*! Represents a textured triangle made up with 3 ua_vertex3d objects. There
-    also is a texnum member to specify used texture.
-*/
-struct ua_triangle3d_textured
-{
-   //! stock texture number used
-   Uint16 texnum;
-
-   //! vertices
-   ua_vertex3d vertices[3];
-
-   //! sets triangle point properties
-   void set(unsigned int point, double x, double y, double z, double u, double v)
-   {
-      vertices[point].pos.set(x,y,z); vertices[point].u = u; vertices[point].v = v;
-   }
-
-   //! compare operator for std::sort
-   bool operator<(const ua_triangle3d_textured& tri) const
-   {
-      return texnum>tri.texnum;
-   }
-};
-
 
 // inline methods
 
@@ -525,5 +481,3 @@ inline double ua_plane3d::signed_dist_to(const ua_vector3d& point) const
 {
    return point.dot(normal) + equation_3;
 }
-
-#endif
