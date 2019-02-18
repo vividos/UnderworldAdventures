329453413f81ad4a562559a21b94548db59c4005
Michael Fink <michael.fink@asamnet.de>
2019-02-18 08:12:11 +0100

    cleaned up uamath and triange3d header files

--- uwadv/source/triangle3d.hpp
+++ uwadv/source/triangle3d.hpp
@@ -16,8 +16,8 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
-/// \file uamath.hpp
-/// \brief math stuff
+/// \file triangle3d.hpp
+/// \brief triangle class
 //
 #pragma once
 
