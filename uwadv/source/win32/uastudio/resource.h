6c48f6f97695df8806afcd4427b42935d7898a9e
Michael Fink <michael.fink@asamnet.de>
2019-02-23 18:35:00 +0100

    cleaned up uastudio files, added Visual Studio 2017 project and added project to solution

--- uwadv/source/win32/uastudio/resource.h
+++ uwadv/source/win32/uastudio/resource.h
@@ -1,16 +1,16 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft Visual C++ generated include file.
 // Used by uastudio_res.rc
 //
-#define IDI_UASTUDIO                    101
+#define IDI_ICON_UASTUDIO               101
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_SYMED_VALUE           102
 #endif
 #endif
