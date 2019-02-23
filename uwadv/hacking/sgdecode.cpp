67f32d09eb397bc56aeafaee47f3608f78fea921
Michael Fink <michael.fink@asamnet.de>
2019-02-23 21:56:03 +0100

    changed savegames decoding according to new data from uw-formats.txt

--- uwadv/hacking/sgdecode.cpp
+++ uwadv/hacking/sgdecode.cpp
@@ -20,10 +20,11 @@
 //
 #include "hacking.h"
 
-static const char *fname = UWPATH "Save1\\player.dat";
+//static const char *fname = UWPATH "Save1\\player.dat";
+static const char *fname = UWPATH "data\\player.dat";
 //static const char *fname = "D:\\uwadv\\uw1\\SAVE4\\player.dat";
 
-char *statsnskills[23] =
+const char* statsnskills[23] =
 {
    "Strength      ",
    "Dexterity     ",
@@ -61,48 +62,59 @@ int sgdecode_main()
       return 1;
    }
 
+   bool isEncrypted = strstr(fname, "data\\player.dat") == NULL;
+
+   unsigned char xorvalue = isEncrypted ? fgetc(fd) : 0;
+
    // read in data
-   unsigned char data[312];
-   fread(data, 1, 312, fd);
+   unsigned char data[311];
+   fread(data, 1, 311, fd);
 
    // descramble data
-   unsigned char xorvalue = data[0];
-   unsigned char incrnum = 3;
-
-   for (int i = 1; i <= 220; i++)
+   if (isEncrypted)
    {
-      if (i == 81) incrnum = 3;
-      data[i] ^= (xorvalue + incrnum);
-      incrnum += 3;
+      unsigned char incrnum = 3;
+
+      for (int i = 0; i <= 219; i++)
+      {
+         if (i == 80 || i == 160) incrnum = 3;
+         data[i] ^= (xorvalue + incrnum);
+         incrnum += 3;
+      }
    }
 
    // print out character info
-   printf("name: %s\n", data + 1);
-   printf("vitality: %u out of %u\n", data[221], data[55]);
-   printf("mana: %u out of %u\n", data[56], data[57]);
-   printf("food: %u, level %u\n", data[58], data[62]);
+   printf("name: %s\n", data);
+   printf("vitality: %u out of %u\n", data[0x0035], data[0x0036]);
+   printf("mana: %u out of %u\n", data[0x0037], data[0x0038]);
+   printf("food: %u, play_level %u\n", data[0x0039], data[0x003D]);
 
-   unsigned int exp_points = data[79] | (unsigned(data[80]) << 8) |
-      (unsigned(data[81]) << 16) | (unsigned(data[82]) << 24);
+   unsigned int exp_points = data[0x004E] | (unsigned(data[0x004F]) << 8) |
+      (unsigned(data[0x0050]) << 16) | (unsigned(data[0x0051]) << 24);
    printf("exp: %4.1f\n", exp_points / 10.f);
 
-   int weight = data[77] | (unsigned(data[78]) << 8);
+   int weight = data[0x004A] | (unsigned(data[0x004B]) << 8);
    printf("weight: %2.1f\n", weight / 10.f);
 
+   printf("position: x=%04x, y=%04x, z=%04x, level=%u\n",
+      (unsigned int)(data[0x0055] | Uint16(data[0x0056] << 8)),
+      (unsigned int)(data[0x0057] | Uint16(data[0x0058] << 8)),
+      (unsigned int)(data[0x0059] | Uint16(data[0x005A] << 8)),
+      (unsigned int)(data[0x005C]));
 
    printf("\nstats & skills:\n");
    for (int n = 0; n < 23; n++)
    {
-      printf("%s: %u\t", statsnskills[n], data[n + 31]);
+      printf("%s: %u\t", statsnskills[n], data[n + 0x001E]);
       if (++n < 23)
-         printf("%s: %u\t", statsnskills[n], data[n + 31]);
+         printf("%s: %u\t", statsnskills[n], data[n + 0x001E]);
       if (++n < 23)
-         printf("%s: %u\t\t", statsnskills[n], data[n + 31]);
+         printf("%s: %u\n", statsnskills[n], data[n + 0x001E]);
    }
    printf("\n");
 
    // dump inventory object list
-   fseek(fd, SEEK_SET, 0x0138);
+   fseek(fd, SEEK_SET, 0x0137);
 
    unsigned short obdata[4];
 
