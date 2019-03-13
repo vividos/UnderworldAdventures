46d70bace4b79312823615917f992a4cc5b5476b
Michael Fink <michael.fink@asamnet.de>
2019-03-13 22:19:17 +0100

    added doxygen project (but only build it in Debug config)

---
+++ uwadv/doxygen/RunDoxygen.cmd
@@ -0,0 +1,10 @@
+REM
+REM Underworld Adventures - an Ultima Underworld hacking project
+REM Copyright (c) 2019 Underworld Adventures Team
+REM
+REM Runs Doxygen to document sourcecode
+REM
+
+doxygen doxygen.cfg
+
+type doxygen-warnings.txt
