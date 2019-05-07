18b6112c70e314bbbaad0ef1310e00be79caf75f
Michael Fink <michael.fink@asamnet.de>
2019-05-07 20:09:30 +0200

    migrated development and old releases page content to new docs folder

--- webpages/irc.php
+++
@@ -1,9 +0,0 @@
-<h2>#uwadv IRC channel</h2>
-
-<p>
-Underworld Adventures has an official IRC channel. Just connect to irc.freenode.net and join #uwadv. Most activity will probably be after 19:00 GMT, as most project members are from europe.
-</p>
-
-<p>
-Exultbot is responsible for the <a href="http://www.math.leidenuniv.nl/~wpalenst/uwadvlog.php" target="_blank">#uwadv channel logs</a>. It also understands some commands. Just say "?help" in the channel to get an overview of the commands.
-</p>
