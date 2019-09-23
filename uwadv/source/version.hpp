358dacbf4b767ce38f3d6058fd6249b9fcf11f23
Michael Fink <michael.fink@asamnet.de>
2019-09-23 22:23:12 +0200

    added version header and version info resources for projects uwadv, uaconfig, uadebug and uastudio

---
+++ uwadv/source/version.hpp
@@ -0,0 +1,36 @@
+//
+// Underworld Adventures - an Ultima Underworld remake project
+// Copyright (c) 2019 Underworld Adventures Team
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+/// \file version.hpp
+/// \brief version number
+//
+#pragma once
+
+#define MAJOR_VERSION 0
+#define MINOR_VERSION 10
+#define RELEASE_NUMBER 0
+#define BUILD_NUMBER 0
+#define BUILD_YEAR 2019
+#define CODENAME "mojito"
+
+#define STRINGIFY(s) STRINGIFY2(s)
+#define STRINGIFY2(s) #s
+
+#define COPYRIGHT_TEXT "Copyright © 2002-" STRINGIFY(BUILD_YEAR) " Underworld Adventures Team"
+
+#define VERSION_TEXT STRINGIFY(MAJOR_VERSION) "." STRINGIFY(MINOR_VERSION) "-" CODENAME
