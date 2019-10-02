90d2d9f4d5b5cb7d1843ae759f5479130adfab56
Michael Fink <michael.fink@asamnet.de>
2019-10-02 20:29:17 +0200

    added readme for data folder and added copying to output folder

---
+++ uwadv/uadata/README.Data.md
@@ -0,0 +1,9 @@
+# Underworld Adventures Data
+
+This folder contains the data for the Underworld Adventures game that is
+needed to run the game.
+
+If you want to listen to the digital music soundtrack, extract the
+uadata00.zip file (this applies only if you're reading this in the binary
+distribution) and then open "Underworld Adventures Soundtrack.m3u" with your
+favourite audio playback software.
--- uwadv/uadata/build.cmd
+++ uwadv/uadata/build.cmd
@@ -15,4 +15,12 @@ mkdir %OUTDIR%data 2> nul
 REM create zip archive
 del %OUTDIR%data\uadata00.zip 2> nul
 
-"c:\Program Files\7-Zip\7z.exe" a -r -x!*.user -x!*.cmd -x!*.lua -x!*.vcxproj* -x!*.user -x!Makefile* %OUTDIR%data\uadata00.zip *.*
+"c:\Program Files\7-Zip\7z.exe" a -r ^
+    -x!*.user -x!*.cmd -x!*.lua -x!*.vcxproj* ^
+    -x!*.user -x!Makefile* ^
+    -x!README.Data.md ^
+    -x!"Underworld Adventures Soundtrack.m3u" ^
+    %OUTDIR%data\uadata00.zip *.*
+
+copy "Underworld Adventures Soundtrack.m3u" %OUTDIR%data\
+copy README.Data.md %OUTDIR%data\README.Data.txt
