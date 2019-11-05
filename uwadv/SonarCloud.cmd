c2d03bff2b3415741ed7a67ef6e0c68c24f03d1e
Michael Fink <michael.fink@asamnet.de>
2019-11-05 08:18:45 +0100

    updated SonarCloud script

--- uwadv/SonarCloud.cmd
+++ uwadv/SonarCloud.cmd
@@ -9,16 +9,19 @@ REM
 REM set this to your Visual Studio installation folder
 set VSINSTALL=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community
 
-REM set this to your SonarQube tools folder
-set SONARQUBE=D:\devel\tools\SonarQube
+REM
+REM Preparations
+REM
+call "%VSINSTALL%\Common7\Tools\VsDevCmd.bat"
 
 if "%SONARLOGIN%" == "" echo "Environment variable SONARLOGIN is not set! Obtain a new token and set the environment variable!"
 if "%SONARLOGIN%" == "" exit 1
 
 REM
-REM Preparations
+REM Prepare SonarQube build tools
 REM
-call "%VSINSTALL%\Common7\Tools\VsDevCmd.bat"
+REM set this to your SonarQube tools folder
+set SONARQUBE=D:\devel\tools\SonarQube
 
 set PATH=%PATH%;%SONARQUBE%\build-wrapper-win-x86;%SONARQUBE%\sonar-scanner-msbuild;%OPENCPPCOVERAGE%
 
@@ -43,5 +46,7 @@ REM
 build-wrapper-win-x86-64.exe --out-dir bw-output msbuild uwadv.sln /m /property:Configuration=SonarCloud,Platform=Win32 /target:Restore;Rebuild
 
 SonarScanner.MSBuild.exe end /d:"sonar.login=%SONARLOGIN%"
+
+:end
 
 pause
