8fb24cc219805aa22064541dc03e29e48ad3d324
Michael Fink <michael.fink@asamnet.de>
2019-11-06 20:16:42 +0100

    added CScintillaEditCommands and CScintillaFindReplaceImpl classes

--- uwadv/source/win32/uadebug/thirdparty/scintilla/atlscintilla.h
+++ uwadv/source/win32/uadebug/thirdparty/scintilla/atlscintilla.h
@@ -37,7 +37,15 @@ History (Date/Author/Description):
 - Fixed signatures of SetSel() and HideSelection(), provided CString variants
   of ReplaceSel() and GetSelText() and added PosFromChar(), to match the CEdit
   class
+- Added CScintillaEditCommands and CScintillaFindReplaceImpl classes
 */
+
+///////////////////////////////////////////////////////////////////////////////
+// Classes in this file:
+//
+// CScintillaWindowT<TBase> - CScintillaWindow
+// CScintillaEditCommands<TBase>
+// CScintillaFindReplaceImpl<TBase>
 
 #pragma once
 #include <vector>
@@ -45,6 +53,7 @@ History (Date/Author/Description):
 #include <Platform.h>
 #include <Scintilla.h>
 #include <atlfile.h>
+#include <atlfind.h>
 
 #pragma comment(lib, "Imm32.lib")
 
@@ -3131,6 +3140,207 @@ public:
 	};
 
 typedef CScintillaWindowT< ATL::CWindow > CScintillaWindow;
+
+/// \brief edit commands mixin for scintilla windows
+/// \details adds handler for "Edit | Redo" command
+template <typename T>
+class CScintillaEditCommands : public CEditCommands<T>
+{
+	/// this class type
+	typedef CScintillaEditCommands<T> ThisClass;
+
+	/// base class type
+	typedef CEditCommands<T> BaseClass;
+
+public:
+	BEGIN_MSG_MAP(ThisClass)
+		ALT_MSG_MAP(1)
+	{
+		// Only perform edit commands when our window has the focus.
+		// This is done to allow multiple windows in a main frame that
+		// can copy/paste/etc. to/from.
+		HWND hWndFocus = ::GetFocus();
+		T* pT = static_cast<T*>(this);
+
+		if (pT->m_hWnd == hWndFocus)
+		{
+			COMMAND_ID_HANDLER(ID_EDIT_REDO, OnEditRedo)
+				CHAIN_MSG_MAP_ALT(BaseClass, 1)
+		}
+	}
+	END_MSG_MAP()
+
+	/// called for command "Edit | Redo"
+	LRESULT OnEditRedo(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+	{
+		T* pT = static_cast<T*>(this);
+
+		pT->Redo();
+		return 0;
+	}
+};
+
+/// \brief find/replace impl for scintilla windows
+/// \details implement FindTextSimple() called by CFindReplaceDialog class.
+/// Also adds message filter, so that tab, escape and other keys work in the dialog.
+template <typename T>
+class CScintillaFindReplaceImpl :
+	public CEditFindReplaceImplBase<T, CFindReplaceDialog>
+{
+	/// this class type
+	typedef CScintillaFindReplaceImpl<T> ThisClass;
+
+	/// base class type
+	typedef CEditFindReplaceImplBase<T, CFindReplaceDialog> BaseClass;
+
+public:
+	/// pre-translates message
+	BOOL PreTranslateMessage(MSG* pMsg)
+	{
+		if (m_pFindReplaceDialog == nullptr)
+			return FALSE;
+
+		T* pT = static_cast<T*>(this);
+
+		HWND hWndFocus = ::GetFocus();
+		if (hWndFocus != nullptr && (
+			(pT->m_hWnd == hWndFocus) || m_pFindReplaceDialog->IsChild(hWndFocus)))
+			return m_pFindReplaceDialog->IsDialogMessage(pMsg);
+
+		return FALSE;
+	}
+
+	/// finds text; used by CEditFindReplaceImplBase
+	BOOL FindTextSimple(LPCTSTR lpszFind, BOOL bMatchCase, BOOL bWholeWord, BOOL bFindDown = TRUE)
+	{
+		T* pT = static_cast<T*>(this);
+
+		Sci_TextToFind tf = { 0 };
+
+		CStringA cszaTextToFind = lpszFind;
+		tf.lpstrText = cszaTextToFind;
+
+		tf.chrg.cpMin = pT->GetSelectionStart();
+		tf.chrg.cpMax = pT->GetSelectionEnd();
+
+		if (m_bFirstSearch)
+		{
+			if (bFindDown)
+				m_nInitialSearchPos = tf.chrg.cpMin;
+			else
+				m_nInitialSearchPos = tf.chrg.cpMax;
+
+			m_bFirstSearch = FALSE;
+		}
+
+		if (tf.chrg.cpMin != tf.chrg.cpMax) // when there's a selection
+		{
+			if (bFindDown)
+				tf.chrg.cpMin++;
+			else
+				// won't wraparound backwards
+				tf.chrg.cpMin = std::max<long>(tf.chrg.cpMin, 0);
+		}
+
+		tf.chrg.cpMax = pT->GetTextLength() + m_nInitialSearchPos;
+
+		if (bFindDown)
+		{
+			if (m_nInitialSearchPos >= 0)
+				tf.chrg.cpMax = pT->GetTextLength();
+		}
+		else
+		{
+			if (m_nInitialSearchPos >= 0)
+				tf.chrg.cpMax = 0;
+		}
+
+		int flags = bMatchCase ? SCFIND_MATCHCASE : 0;
+		flags |= bWholeWord ? SCFIND_WHOLEWORD : 0;
+
+		bool bRet = FindAndSelect(flags, tf);
+
+		if (!bRet && m_nInitialSearchPos > 0)
+		{
+			// if the original starting point was not the beginning
+			// of the buffer and we haven't already been here
+			if (bFindDown)
+			{
+				tf.chrg.cpMin = 0;
+				tf.chrg.cpMax = m_nInitialSearchPos;
+			}
+			else
+			{
+				tf.chrg.cpMin = pT->GetTextLength();
+				tf.chrg.cpMax = m_nInitialSearchPos;
+			}
+			m_nInitialSearchPos = m_nInitialSearchPos - pT->GetTextLength();
+
+			bRet = FindAndSelect(flags, tf);
+		}
+
+		return bRet ? TRUE : FALSE;
+	}
+
+	/// finds and selects next text
+	bool FindAndSelect(int flags, Sci_TextToFind& tf)
+	{
+		T* pT = static_cast<T*>(this);
+
+		unsigned int uiRet = pT->FindText(flags, tf);
+		bool bRet = uiRet != (unsigned int)-1;
+
+		if (bRet)
+			pT->SetSel(tf.chrgText.cpMin, tf.chrgText.cpMax);
+
+		return bRet;
+	}
+
+private:
+	BEGIN_MSG_MAP(ThisClass)
+		ALT_MSG_MAP(1)
+		COMMAND_ID_HANDLER(ID_EDIT_CUT, OnEditCommand)
+		COMMAND_ID_HANDLER(ID_EDIT_COPY, OnEditCommand)
+		COMMAND_ID_HANDLER(ID_EDIT_PASTE, OnEditCommand)
+		COMMAND_ID_HANDLER(ID_EDIT_SELECT_ALL, OnEditCommand)
+		COMMAND_ID_HANDLER(ID_EDIT_UNDO, OnEditCommand)
+		COMMAND_ID_HANDLER(ID_EDIT_REDO, OnEditCommand)
+		CHAIN_MSG_MAP_ALT(BaseClass, 1)
+	END_MSG_MAP()
+
+	/// called for edit commands
+	LRESULT OnEditCommand(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& bHandled)
+	{
+		if (m_pFindReplaceDialog == nullptr)
+		{
+			bHandled = false;
+			return 0;
+		}
+
+		HWND hWndFocus = ::GetFocus();
+		if (!m_pFindReplaceDialog->IsChild(hWndFocus))
+		{
+			bHandled = false;
+			return 0;
+		}
+
+		CWindow wnd(hWndFocus);
+		switch (wID)
+		{
+		case ID_EDIT_CUT:    wnd.SendMessage(WM_CUT); break;
+		case ID_EDIT_COPY:   wnd.SendMessage(WM_COPY); break;
+		case ID_EDIT_PASTE:  wnd.SendMessage(WM_PASTE); break;
+		case ID_EDIT_SELECT_ALL:   wnd.SendMessage(EM_SETSEL, 0, -1); break;
+		case ID_EDIT_UNDO:   wnd.SendMessage(EM_UNDO); break;
+		case ID_EDIT_REDO:   wnd.SendMessage(EM_REDO); break;
+		default:
+			ATLASSERT(false);
+			break;
+		}
+
+		return 0;
+	}
+};
 
 class CScintillaAutoRegister
 {
