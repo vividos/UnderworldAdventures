c957f408372e9da00a37bdb39917ec40017c8579
Michael Fink <michael.fink@asamnet.de>
2020-07-26 17:57:47 +0200

    added running unit tests and collecting coverage during Sonar analysis

--- uwadv/SonarCloud.cmd
+++ uwadv/SonarCloud.cmd
@@ -12,6 +12,9 @@ set VSINSTALL=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community
 REM set this to your SonarQube tools folder
 set SONARQUBE=C:\Projekte\Tools\SonarQube
 
+REM and this to your OpenCppCoverage folder
+set OPENCPPCOVERAGE=C:\Projekte\Tools\OpenCppCoverage\
+
 REM
 REM Preparations
 REM
@@ -32,15 +35,38 @@ SonarScanner.MSBuild.exe begin ^
     /k:"UnderworldAdventures" ^
     /v:"0.10" ^
     /d:"sonar.cfamily.build-wrapper-output=%CD%\bw-output" ^
+    /d:"sonar.coverageReportPaths=%CD%\CoverageReport-SonarQube.xml" ^
     /d:"sonar.host.url=https://sonarcloud.io" ^
     /o:"vividos-github" ^
-    /d:"sonar.login=%SONARLOGIN%"
+    /d:"sonar.login=%SONARLOGIN%" ^
+    /d:sonar.cs.vstest.reportsPaths="%CD%\TestResults\*.trx"
 if errorlevel 1 goto end
 
 REM
 REM Rebuild Release|Win32
 REM
 build-wrapper-win-x86-64.exe --out-dir bw-output msbuild uwadv.sln /m /property:Configuration=SonarCloud,Platform=Win32 /target:Restore;Rebuild
+
+REM
+REM Run unit tests
+REM
+OpenCppCoverage.exe ^
+   --continue_after_cpp_exception --cover_children ^
+   --sources source\unittest ^
+   --sources source\base ^
+   --sources source\audio ^
+   --sources source\import ^
+   --sources source\physics ^
+   --sources source\underw ^
+   --excluded_sources packages ^
+   --excluded_sources source\thirdparty ^
+   --excluded_sources source\audio\midi ^
+   --export_type SonarQube:CoverageReport-SonarQube.xml ^
+   --export_type html:CoverageReport ^
+   --modules unittest.dll ^
+   --excluded_modules vstest.console.exe ^
+   -- "%VSINSTALL%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" ^
+   "output\bin\Release\unittest.dll" /Platform:x86 /InIsolation /logger:trx
 
 SonarScanner.MSBuild.exe end /d:"sonar.login=%SONARLOGIN%"
 
