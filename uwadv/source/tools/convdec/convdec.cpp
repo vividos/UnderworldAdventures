cad9a1b892c90afd7f4c7412ada536bef189c0b3
Michael Fink <michael.fink@asamnet.de>
2021-09-05 14:53:51 +0200

    moved Decompiler class to own source files

--- uwadv/source/tools/convdec/convdec.cpp
+++ uwadv/source/tools/convdec/convdec.cpp
@@ -19,157 +19,12 @@
 /// \file convdec.cpp
 /// \brief Underworld conversation code decompiler
 //
-#include "Conversation.hpp"
-#include "convdec.hpp"
-#include "CodeGraph.hpp"
-#include "ConvLoader.hpp"
+#include "Decompiler.hpp"
 #include "GameStrings.hpp"
 #include "GameStringsImporter.hpp"
-#include "Settings.hpp"
-#include "ResourceManager.hpp"
 #include "FileSystem.hpp"
 
-using namespace Conv;
-
-Decompiler::Decompiler(Uint16 conversationNumber, std::string basePath, GameStrings& strings)
-   :m_conversationNumber(conversationNumber),
-   m_strings(strings)
-{
-   Conv::CodeVM codeVM;
-
-   Base::Settings settings;
-   settings.SetValue(Base::settingUnderworldPath, basePath);
-   Base::ResourceManager resourceManager{ settings };
-
-   if (Base::FileSystem::FileExists(basePath + "uw2.exe"))
-      settings.SetGameType(Base::gameUw2);
-
-   if (!Import::LoadConvCode(codeVM, settings, resourceManager, "data/cnv.ark", conversationNumber))
-      return;
-
-   std::vector<std::string> stringBlock = strings.GetStringBlock(0x0e00 + conversationNumber);
-
-   m_codeGraph.reset(new Conv::CodeGraph(codeVM.GetCodeSegment(),
-      0,
-      static_cast<Uint16>(codeVM.GetCodeSegment().size()),
-      stringBlock,
-      codeVM.GetImportedFunctions(),
-      codeVM.GetImportedGlobals(),
-      codeVM.GetReservedGlobals()));
-
-   m_codeGraph->Decompile();
-}
-
-std::string Decompiler::GetName() const
-{
-   return m_strings.GetString(7, m_conversationNumber + 16);
-}
-
-void Decompiler::Write(FILE* fd, bool showDisassembly)
-{
-   fputs("; conversation for ", fd);
-   fputs(m_strings.GetString(7, m_conversationNumber + 16).c_str(), fd);
-   fputs("\n\n", fd);
-   fflush(fd);
-
-   if (m_codeGraph == NULL ||
-      m_codeGraph->GetGraph().empty())
-   {
-      fputs("; conversation contained no code\n", fd);
-      fflush(fd);
-      return;
-   }
-
-   int indentLevel = 0;
-   const Conv::CodeGraph::GraphList& g = m_codeGraph->GetGraph();
-   Conv::CodeGraph::const_graph_iterator iter = g.begin(), stop = g.end();
-   for (; iter != stop; ++iter)
-   {
-      if (showDisassembly)
-      {
-         // display label names that have xrefs
-         if (iter->m_isProcessed && iter->m_labelName.length() != 0 && iter->m_xrefCount > 0)
-         {
-            fputs(iter->m_labelName.c_str(), fd);
-            fputs(":\n", fd);
-         }
-
-         if (iter->m_type != Conv::typeOpcode &&
-            iter->m_type != Conv::typeStatement)
-            continue;
-      }
-      else
-      {
-         if (iter->m_isProcessed)
-            continue;
-      }
-
-      if (iter->m_type == Conv::typeStatement && iter->statement_data.indent_change_before != 0)
-         indentLevel += iter->statement_data.indent_change_before;
-      UaAssert(indentLevel >= 0);
-
-      if (iter->m_type == Conv::typeFuncStart)
-      {
-         std::string funcName = iter->m_labelName;
-
-         if (funcName.find("unused_") == 0 || iter->m_xrefCount == 0)
-         {
-            while (iter != stop && iter->m_type != Conv::typeFuncEnd)
-               ++iter;
-
-            // jump over opcodes from function end
-            std::advance(iter, 3);
-
-            continue;
-         }
-      }
-
-      // show any unprocessed labels
-      if (!iter->m_isProcessed &&
-         iter->m_labelName.length() != 0)
-      {
-         fputs(iter->m_labelName.c_str(), fd);
-         fputs(":\n", fd);
-      }
-
-      for (int i = 0; i < indentLevel; i++)
-         fputs("   ", fd);
-
-      std::string text = iter->Format();
-      fputs(text.c_str(), fd);
-
-      // show cross-references in disassembly mode
-      if (showDisassembly && iter->m_type == Conv::typeFuncStart)
-      {
-         std::string funcName = iter->m_labelName;
-
-         const Conv::CodeGraph::FunctionMap& functionMap = m_codeGraph->GetFunctionMap();
-
-         UaAssert(functionMap.find(funcName) != functionMap.end());
-
-         const Conv::FuncInfo& funcInfo = functionMap.find(funcName)->second;
-
-         if (!funcInfo.caller.empty())
-         {
-            fputs(", caller xrefs: ", fd);
-            std::for_each(funcInfo.caller.begin(), funcInfo.caller.end(), [fd](const std::string& caller)
-            {
-               fputs(caller.c_str(), fd);
-               fputs(", ", fd);
-            });
-         }
-      }
-
-      fputs("\n", fd);
-      fflush(fd);
-
-      if (iter->m_type == Conv::typeStatement && iter->statement_data.indent_change_after != 0)
-         indentLevel += iter->statement_data.indent_change_after;
-      UaAssert(indentLevel >= 0);
-   }
-}
-
-
+/// convdec tool main function
 int main(int argc, char* argv[])
 {
    printf("convdec - Ultima Underworld conversation code decompiler\n"
@@ -200,7 +55,7 @@ int main(int argc, char* argv[])
    bool showDisassembly = true;
 
    GameStrings gameStrings;
-   Import::GameStringsImporter importer(gameStrings);
+   Import::GameStringsImporter importer{ gameStrings };
    importer.LoadStringsPakFile((basePath + "data/strings.pak").c_str());
 
    if (slotText != "*")
