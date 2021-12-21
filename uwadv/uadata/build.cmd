86e0cb0949dbc843ca674325c8abf07b368a9eac
Michael Fink <michael.fink@asamnet.de>
2021-12-21 08:02:47 +0100

    don't add CMakeLists.txt to uadata00.zip

--- uwadv/uadata/build.cmd
+++ uwadv/uadata/build.cmd
@@ -1,6 +1,6 @@
 REM
 REM Underworld Adventures - an Ultima Underworld remake project
-REM Copyright (c) 2019 Underworld Adventures Team
+REM Copyright (c) 2019,2021 Underworld Adventures Team
 REM
 REM Build script for uadata zip archive
 REM
@@ -17,7 +17,7 @@ del %OUTDIR%data\uadata00.zip 2> nul
 
 "c:\Program Files\7-Zip\7z.exe" a -r ^
     -x!*.user -x!*.cmd -x!*.lua -x!*.vcxproj* ^
-    -x!*.user -x!Makefile* ^
+    -x!*.user -x!CMakeLists.txt ^
     -x!README.Data.md ^
     -x!"Underworld Adventures Soundtrack.m3u" ^
     %OUTDIR%data\uadata00.zip *.*
