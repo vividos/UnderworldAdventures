5cd1e8e61c65b920383a6056eac65489dac45bae
Michael Fink <michael.fink@asamnet.de>
2022-08-29 21:59:25 +0200

    modified android project to match an Android Studio generated project
    also adjusted project settings for uwadv

---
+++ uwadv/android/app/src/main/java/de/vividos/app/underworld_adventures/android/GameActivity.java
@@ -0,0 +1,12 @@
+package de.vividos.app.underworld_adventures.android;
+
+import android.os.Bundle;
+import org.libsdl.app.SDLActivity;
+
+public class GameActivity extends SDLActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
