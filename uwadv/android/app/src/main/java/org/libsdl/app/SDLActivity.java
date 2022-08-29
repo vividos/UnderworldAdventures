9cba68c1183520bb2f6ea63b0b202154a2ce3e9d
Michael Fink <michael.fink@asamnet.de>
2022-08-29 22:19:00 +0200

    added copying external libSDL2.so and libSDL2_mixer.so from vcpkg build

--- uwadv/android/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ uwadv/android/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -274,7 +274,7 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
         return new String[] {
             "SDL2",
             // "SDL2_image",
-            // "SDL2_mixer",
+            "SDL2_mixer",
             // "SDL2_net",
             // "SDL2_ttf",
             "main"
