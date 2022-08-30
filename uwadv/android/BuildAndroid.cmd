d38aea03c25ea2d6e55016e170e0f1dca2c2fa65
Michael Fink <michael.fink@asamnet.de>
2022-08-30 20:17:21 +0200

    added cmd script to build Android app

---
+++ uwadv/android/BuildAndroid.cmd
@@ -0,0 +1,22 @@
+@echo off
+REM
+REM Underworld Adventures - an Ultima Underworld hacking project
+REM Copyright (c) 2022 Underworld Adventures Team
+REM
+REM Builds Android app
+REM
+
+REM set this to your installed Android NDK
+set ANDROID_NDK_HOME=C:\Projekte\Tools\AndroidSdk\ndk\24.0.8215888
+
+REM set this to your vcpkg git repository
+set VCPKG_ROOT=C:\Projekte\Tools\vcpkg
+
+pushd ..
+
+cmake -B output\build\android-arm64-v8a-debug
+cmake --build ^
+  --preset "android-arm64-v8a-debug" ^
+  --target "uwadv_android_apk"
+
+popd
