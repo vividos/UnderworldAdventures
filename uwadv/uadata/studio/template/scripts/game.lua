0d8755470239517a15e7b504991c5e5f48cfa216
Michael Fink <michael.fink@asamnet.de>
2022-08-30 21:09:10 +0200

    added template files for uastudio to create new games

---
+++ uwadv/uadata/studio/template/scripts/game.lua
@@ -0,0 +1,18 @@
+--
+-- Underworld Adventures - an Ultima Underworld remake project
+-- Copyright (c) 2002-2022 Underworld Adventures Team
+--
+-- game.lua - Lua custom game script
+--
+
+-- called when a new game is started, e.g. after character generation
+function game_init_new()
+
+   print("Lua: game_init_new() called")
+
+end
+
+-- called when changing to a new level
+function on_change_level()
+
+end
