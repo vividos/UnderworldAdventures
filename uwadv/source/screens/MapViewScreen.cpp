98f15e71b3b72882c425a0dcfec9197cc1116e1b
Michael Fink <michael.fink@asamnet.de>
2023-04-07 15:53:37 +0200

    implemented adding and deleting map notes on map view screen

--- uwadv/source/screens/MapViewScreen.cpp
+++ uwadv/source/screens/MapViewScreen.cpp
@@ -1,6 +1,6 @@
 //
 // Underworld Adventures - an Ultima Underworld remake project
-// Copyright (c) 2002,2003,2019,2021,2022 Underworld Adventures Team
+// Copyright (c) 2002,2003,2019,2021,2022,2023 Underworld Adventures Team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -34,33 +34,50 @@ MapViewScreen::MapViewScreen(IGame& gameInterface)
    m_upButton(*this,
       std::bind(&MapViewScreen::UpDownLevel, this, true)),
    m_downButton(*this,
-      std::bind(&MapViewScreen::UpDownLevel, this, false))
+      std::bind(&MapViewScreen::UpDownLevel, this, false)),
+   m_eraseButton(*this,
+      std::bind(&MapViewScreen::SetMapEditMode, this, MapEditMode::mapEditModeErase)),
+   m_closeButton(*this,
+      std::bind(&MapViewScreen::StartFadeout, this))
 {
+   m_fontSelectedMapNote.Load(
+      gameInterface.GetGameInstance().GetResourceManager(), fontSmall);
 }
 
 void MapViewScreen::Init()
 {
    ImageScreen::Init();
 
+   // add selection image here so that it's always below the mouse cursor
+   m_selectedMapNote = std::make_shared<ImageQuad>();
+   RegisterWindow(m_selectedMapNote.get());
+
    // init mouse cursor
    m_mouseCursor.Init(m_gameInstance, 0);
    m_mouseCursor.Show(true);
    RegisterWindow(&m_mouseCursor);
+   SetMapEditMode(MapEditMode::mapEditModeNone);
 
-   // init click areas
+   // init click areas and buttons
    RegisterWindow(&m_upButton);
    RegisterWindow(&m_downButton);
+   RegisterWindow(&m_eraseButton);
+   RegisterWindow(&m_closeButton);
 
    bool isUw2 = m_gameInstance.GetSettings().IsGameUw2();
    if (!isUw2)
    {
       m_upButton.Create(290, 0, 30, 22);
       m_downButton.Create(290, 178, 30, 22);
+      m_eraseButton.Create(272, 118, 29, 26);
+      m_closeButton.Create(263, 150, 48, 28);
    }
    else
    {
       m_upButton.Create(284, 0, 36, 32);
       m_downButton.Create(284, 168, 36, 32);
+      m_eraseButton.Create(264, 109, 29, 26);
+      m_closeButton.Create(255, 118, 48, 28);
    }
 
    m_displayedLevel = m_gameInstance.GetUnderworld().GetPlayer().GetAttribute(Underworld::attrMapLevel);
@@ -73,14 +90,67 @@ void MapViewScreen::Init()
 
 bool MapViewScreen::ProcessEvent(SDL_Event& event)
 {
+   // while entering text, ignore cursor moves
+   if (event.type == SDL_MOUSEMOTION &&
+      m_mapEditMode == MapEditMode::mapEditModeEditNote)
+      return true;
+
    if (ImageScreen::ProcessEvent(event))
       return true;
+
+   bool isHandled = false;
 
    switch (event.type)
    {
    case SDL_MOUSEBUTTONDOWN:
-      StartFadeout();
+   {
+      int xpos = event.button.x;
+      int ypos = event.button.y;
+      MapWindowPosition(xpos, ypos);
+
+      if (m_mapEditMode == MapEditMode::mapEditModeNone)
+      {
+         ypos -= 7;
+
+         // no notes on the far side
+         if (xpos < 256)
+         {
+            StartEditing((unsigned int)xpos, (unsigned int)ypos);
+            isHandled = true;
+         }
+      }
+      else if (m_mapEditMode == MapEditMode::mapEditModeErase)
+      {
+         if (xpos < 256)
+            EraseMapNote(xpos, ypos);
+
+         RemoveMapNoteSelectionImage();
+
+         SetMapEditMode(MapEditMode::mapEditModeNone);
+         isHandled = true;
+      }
       break;
+   }
+
+   case SDL_MOUSEMOTION:
+   {
+      int xpos = event.motion.x;
+      int ypos = event.motion.y;
+      MapWindowPosition(xpos, ypos);
+
+      if (m_mapEditMode == MapEditMode::mapEditModeNone)
+      {
+         // use crosshair cursor when outside editing area
+         m_mouseCursor.SetType(xpos < 256 ? 12 : 0);
+         isHandled = true;
+      }
+      else if (m_mapEditMode == MapEditMode::mapEditModeErase)
+      {
+         SelectMapNote(xpos, ypos);
+         isHandled = true;
+      }
+      break;
+   }
 
    case SDL_KEYDOWN:
       // handle key presses
@@ -89,7 +159,19 @@ bool MapViewScreen::ProcessEvent(SDL_Event& event)
       case SDLK_SPACE:
       case SDLK_RETURN:
       case SDLK_ESCAPE:
-         StartFadeout();
+         if (m_mapEditMode == MapEditMode::mapEditModeNone)
+         {
+            StartFadeout();
+            isHandled = true;
+         }
+         else if (m_mapEditMode == MapEditMode::mapEditModeErase)
+         {
+            SetMapEditMode(MapEditMode::mapEditModeNone);
+            RemoveMapNoteSelectionImage();
+            isHandled = true;
+         }
+
+         // leaving mapEditModeEditNote is handled by user events
          break;
 
       default:
@@ -98,12 +180,46 @@ bool MapViewScreen::ProcessEvent(SDL_Event& event)
       }
       break;
 
+   case SDL_USEREVENT:
+      if (m_textEditWindow != nullptr)
+      {
+         if (event.user.code == gameEventTexteditFinished)
+         {
+            AddNewMapNote(
+               m_textEditWindow->GetXPos(),
+               m_textEditWindow->GetYPos(),
+               m_textEditWindow->GetText());
+         }
+
+         m_textEditWindow->SetCursorPosChangedHandler();
+
+         UnregisterWindow(m_textEditWindow.get());
+         m_textEditWindow.reset();
+
+         SetMapEditMode(MapEditMode::mapEditModeNone);
+         isHandled = true;
+      }
+      break;
+
    default:
       // ignore other events
       break;
    }
 
-   return true;
+   return isHandled;
+}
+
+void MapViewScreen::SetMapEditMode(MapEditMode mapEditMode)
+{
+   m_mapEditMode = mapEditMode;
+   unsigned int cursorIndex = 12;
+   if (mapEditMode == MapEditMode::mapEditModeEditNote)
+      cursorIndex = 14;
+
+   if (mapEditMode == MapEditMode::mapEditModeErase)
+      cursorIndex = 13;
+
+   m_mouseCursor.SetType(cursorIndex);
 }
 
 void MapViewScreen::UpDownLevel(bool up)
@@ -168,4 +284,156 @@ void MapViewScreen::DisplayLevelMap(size_t levelIndex)
       generator.DrawPlayerPin(image, player);
 
    UpdateImage();
+}
+
+void MapViewScreen::StartEditing(unsigned int xpos, unsigned int ypos)
+{
+   SetMapEditMode(MapEditMode::mapEditModeEditNote);
+   m_textEditWindow = std::make_shared<TextEditWindow>();
+
+   bool isUw2 = m_gameInstance.GetSettings().IsGameUw2();
+
+   Uint8 textColor = !isUw2 ? 45 : 78;
+   m_textEditWindow->Init(m_game.GetGameInstance(), xpos - 1, ypos + 7,
+      320 - xpos, 0, 0, textColor, "", "", false, fontSmall, 1);
+
+   m_editStartCursorPosX = xpos - 1;
+
+   m_textEditWindow->SetCursorPosChangedHandler(
+      [&](unsigned int cursorPosX)
+      {
+         m_mouseCursor.SetXPos(m_editStartCursorPosX + cursorPosX);
+      });
+
+   RegisterWindow(m_textEditWindow.get());
+}
+
+void MapViewScreen::AddNewMapNote(unsigned int xpos, unsigned int ypos, std::string noteText)
+{
+   Underworld::MapNotes& mapNotes =
+      m_game.GetGameInstance().GetUnderworld().GetLevelList()
+      .GetLevel(m_displayedLevel).GetMapNotes();
+
+   Underworld::MapNote mapNote;
+   mapNote.m_xpos = xpos;
+   mapNote.m_ypos = ypos;
+   mapNote.m_text = noteText;
+
+   mapNotes.GetMapNotesList().push_back(mapNote);
+
+   DisplayLevelMap(m_displayedLevel);
+}
+
+void MapViewScreen::SelectMapNote(unsigned int xpos, unsigned int ypos)
+{
+   std::vector<Underworld::MapNote>::const_iterator iter =
+      FindMapNote(xpos, ypos);
+
+   const Underworld::MapNotes& mapNotes =
+      m_game.GetGameInstance().GetUnderworld().GetLevelList()
+      .GetLevel(m_displayedLevel).GetMapNotes();
+
+   if (iter == mapNotes.GetMapNotesList().end())
+   {
+      RemoveMapNoteSelectionImage();
+      return;
+   }
+
+   std::vector<Underworld::MapNote>::difference_type mapNotesIndex =
+      std::distance(mapNotes.GetMapNotesList().begin(), iter);
+
+   if (mapNotesIndex == m_selectedMapNotesIndex)
+      return; // skip recreating the same select item
+
+   m_selectedMapNotesIndex = mapNotesIndex;
+
+   const Underworld::MapNote& mapNoteToSelect = *iter;
+
+   CreateMapNoteSelectionImage(mapNoteToSelect,
+      m_selectedMapNote->GetImage());
+
+   m_selectedMapNote->Init(m_game.GetGameInstance(),
+      mapNoteToSelect.m_xpos - 1, mapNoteToSelect.m_ypos - 1);
+   m_selectedMapNote->Update();
+}
+
+void MapViewScreen::CreateMapNoteSelectionImage(const Underworld::MapNote& mapNoteToSelect,
+   IndexedImage& selectedMapNoteImage)
+{
+   Uint8 selectedColor = 48; // blue
+
+   IndexedImage mapNoteTextImage;
+   m_fontSelectedMapNote.CreateString(
+      mapNoteTextImage,
+      mapNoteToSelect.m_text, selectedColor);
+
+   selectedMapNoteImage.Create(
+      mapNoteTextImage.GetXRes() + 3, mapNoteTextImage.GetYRes() + 4);
+
+   selectedMapNoteImage.Clear(selectedColor);
+   selectedMapNoteImage.FillRect(1, 1,
+      selectedMapNoteImage.GetXRes() - 2, selectedMapNoteImage.GetYRes() - 2,
+      0);
+
+   selectedMapNoteImage.PasteImage(mapNoteTextImage, 2, 2, false);
+}
+
+void MapViewScreen::EraseMapNote(unsigned int xpos, unsigned int ypos)
+{
+   std::vector<Underworld::MapNote>::const_iterator iter =
+      FindMapNote(xpos, ypos);
+
+   Underworld::MapNotes& mapNotes =
+      m_game.GetGameInstance().GetUnderworld().GetLevelList()
+      .GetLevel(m_displayedLevel).GetMapNotes();
+
+   if (iter == mapNotes.GetMapNotesList().end())
+      return;
+
+   mapNotes.GetMapNotesList().erase(iter);
+
+   RemoveMapNoteSelectionImage();
+
+   DisplayLevelMap(m_displayedLevel);
+}
+
+void MapViewScreen::RemoveMapNoteSelectionImage()
+{
+   m_selectedMapNotesIndex = -1;
+
+   m_selectedMapNote->GetImage().Create(1, 1);
+   m_selectedMapNote->Init(m_game.GetGameInstance(), 320, 200);
+}
+
+std::vector<Underworld::MapNote>::const_iterator MapViewScreen::FindMapNote(
+   unsigned int xpos, unsigned int ypos)
+{
+   Underworld::MapNotes& mapNotes =
+      m_game.GetGameInstance().GetUnderworld().GetLevelList()
+      .GetLevel(m_displayedLevel).GetMapNotes();
+
+   unsigned int height = m_fontSelectedMapNote.GetCharHeight();
+
+   size_t maxIndex = mapNotes.GetMapNoteCount();
+   for (size_t noteIndex = 0; noteIndex < maxIndex; noteIndex++)
+   {
+      const Underworld::MapNote& mapNote = mapNotes.GetNote(noteIndex);
+
+      if (xpos < mapNote.m_xpos ||
+         ypos < mapNote.m_ypos ||
+         ypos >= mapNote.m_ypos + height)
+         continue;
+
+      unsigned int width = m_fontSelectedMapNote.CalcLength(mapNote.m_text);
+
+      if (xpos >= mapNote.m_xpos + width)
+         continue;
+
+      std::vector<Underworld::MapNote>::const_iterator foundIter =
+         mapNotes.GetMapNotesList().begin() + noteIndex;
+
+      return foundIter;
+   }
+
+   return mapNotes.GetMapNotesList().end();
 }
--- uwadv/source/screens/MapViewScreen.hpp
+++ uwadv/source/screens/MapViewScreen.hpp
@@ -1,6 +1,6 @@
 //
 // Underworld Adventures - an Ultima Underworld remake project
-// Copyright (c) 2002,2003,2019,2021,2022 Underworld Adventures Team
+// Copyright (c) 2002,2003,2019,2021,2022,2023 Underworld Adventures Team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -24,6 +24,12 @@
 #include "ImageScreen.hpp"
 #include "MouseCursor.hpp"
 #include "ClickArea.hpp"
+#include "TextEditWindow.hpp"
+
+namespace Underworld
+{
+   struct MapNote;
+}
 
 /// level map view screen class
 class MapViewScreen : public ImageScreen
@@ -39,14 +45,52 @@ public:
    virtual bool ProcessEvent(SDL_Event& event) override;
 
 private:
+   /// mode of map editing
+   enum MapEditMode
+   {
+      mapEditModeNone,     ///< no editing yet
+      mapEditModeEditNote, ///< editing a map note
+      mapEditModeErase,    ///< erasing mode
+   };
+
+   /// sets new map edit mode
+   void SetMapEditMode(MapEditMode mapEditMode);
+
    /// changes the displayed level up or down
    void UpDownLevel(bool up);
 
    /// displays level map with given index
    void DisplayLevelMap(size_t levelIndex);
 
+   /// starts editing a new map note
+   void StartEditing(unsigned int xpos, unsigned int ypos);
+
+   /// adds new map note
+   void AddNewMapNote(unsigned int xpos, unsigned int ypos, std::string noteText);
+
+   /// selects a map note by drawing a border around it
+   void SelectMapNote(unsigned int xpos, unsigned int ypos);
+
+   /// creates an image with a selected map note text
+   void CreateMapNoteSelectionImage(const Underworld::MapNote& mapNoteToSelect,
+      IndexedImage& selectedMapNoteImage);
+
+   /// erases map note at given position
+   void EraseMapNote(unsigned int xpos, unsigned int ypos);
+
+   /// removes map note selection image from screen
+   void RemoveMapNoteSelectionImage();
+
+   /// finds a map note at given position; returns end iterator when none was found
+   std::vector<Underworld::MapNote>::const_iterator
+      FindMapNote(unsigned int xpos, unsigned int ypos);
+
+private:
    /// currently displayed level
    size_t m_displayedLevel;
+
+   /// current map edit mode
+   MapEditMode m_mapEditMode = mapEditModeNone;
 
    /// mouse cursor
    MouseCursor m_mouseCursor;
@@ -56,4 +100,26 @@ private:
 
    /// down button click area
    ClickArea m_downButton;
+
+   /// erase button click area
+   ClickArea m_eraseButton;
+
+   /// close button click area
+   ClickArea m_closeButton;
+
+   /// text edit window
+   std::shared_ptr<TextEditWindow> m_textEditWindow;
+
+   /// cursor X position when starting editing
+   unsigned int m_editStartCursorPosX = 0;
+
+   /// font for selected map notes
+   Font m_fontSelectedMapNote;
+
+   /// image quad containing the currently selected map note, or nullptr when
+   /// none is selected
+   std::shared_ptr<ImageQuad> m_selectedMapNote;
+
+   /// index of currently selected map note
+   std::vector<Underworld::MapNote>::difference_type m_selectedMapNotesIndex = -1;
 };
